9cc31212884ca07a94ea23a8b11fb5d65d242097
Karim <devkarim@hotmail.com>
2023-09-02 01:04:50 +0300

    feat(web): add login page

---
+++ apps/web/src/app/(routes)/login/page.tsx
@@ -0,0 +1,40 @@
+import { Metadata } from 'next';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Anchor from '@/components/ui/anchor';
+import LoginForm from './components/login-form';
+
+interface LoginPageProps {}
+
+export const metadata: Metadata = {
+  title: 'Discord Clone - Login',
+  description: 'Log into your Discord account.',
+};
+
+const LoginPage: React.FC<LoginPageProps> = ({}) => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center p-2">
+      <Card className="border-0 w-full max-w-xl">
+        <CardHeader className="text-center space-y-4">
+          <CardTitle>Welcome back!</CardTitle>
+          <CardDescription>We're so excited to see you again!</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-3">
+          <LoginForm />
+          <p className="text-sm">
+            <span className="opacity-50">Need an account?</span>{' '}
+            <Anchor href="/register">Register</Anchor>
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default LoginPage;
