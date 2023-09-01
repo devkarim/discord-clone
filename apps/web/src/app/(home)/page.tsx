5991345ec866c30ae6df0f9eb18452cddd6fc93a
Karim <devkarim@hotmail.com>
2023-09-01 03:41:46 +0300

    refactor(web): test prisma

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -1,6 +1,12 @@
+import prisma from '@/lib/prisma';
+
 export default async function Page() {
+  const users = await prisma.user.findMany();
   return (
     <div>
+      {users.map((user) => (
+        <p key={user.id}>{user.email}</p>
+      ))}
       <p>hello</p>
     </div>
   );
