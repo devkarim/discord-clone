e427225d5aa6feb772097f896fe2ddae1a66c6f3
Karim <devkarim@hotmail.com>
2023-09-01 04:38:39 +0300

    fix(backend): change production domain

--- apps/backend/src/config/constants.ts
+++ apps/backend/src/config/constants.ts
@@ -1,9 +1,11 @@
-import { env } from "./env";
+import { env } from './env';
 
-export const isDevelopment = env.NODE_ENV === "development";
-export const isProduction = env.NODE_ENV === "production";
+export const isDevelopment = env.NODE_ENV === 'development';
+export const isProduction = env.NODE_ENV === 'production';
 
-export const API_DOMAIN = isDevelopment ? "localhost" : "api.example.com";
+export const API_DOMAIN = isDevelopment
+  ? 'localhost'
+  : 'api.discord-clone.karimwael.com';
 export const API_URL = isDevelopment
   ? `http://${API_DOMAIN}:8000`
   : `https://${API_DOMAIN}`;
