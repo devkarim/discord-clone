cd991fac0afc3d255b1c9c800ea06ca029126db4
Karim <devkarim@hotmail.com>
2023-09-02 03:56:54 +0300

    fix(backend): add redis_database_url to env

--- apps/backend/src/config/env.ts
+++ apps/backend/src/config/env.ts
@@ -1,16 +1,17 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(8000),
   SESSION_SECRET: z.string(),
-  NODE_ENV: z.union([z.literal("development"), z.literal("production")]),
+  NODE_ENV: z.union([z.literal('development'), z.literal('production')]),
+  REDIS_DATABASE_URL: z.string(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
 
 if (!parsedEnv.success) {
   console.error(parsedEnv.error.issues);
-  throw new Error("There is an error with the environment variables");
+  throw new Error('There is an error with the environment variables');
 }
 
 export const env = parsedEnv.data;
