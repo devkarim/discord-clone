21096a1f8c02c5ea9db6cc73c52ddcfc69784c3f
Karim <devkarim@hotmail.com>
2023-09-02 03:58:10 +0300

    feat(web): add session functions for serverside

---
+++ apps/web/src/actions/session.ts
@@ -0,0 +1,31 @@
+import { cookies } from 'next/headers';
+
+import { BaseResponse } from 'models';
+import { User } from 'database';
+
+import client from './client';
+
+type UserResponse = BaseResponse<Omit<User, 'password'>>;
+
+export const getSession = () => {
+  const cookieStore = cookies();
+  const sessionId = cookieStore.get('connect.sid');
+  return sessionId?.value;
+};
+
+export const getUser = async () => {
+  const sessionId = getSession();
+  if (!sessionId) return null;
+  try {
+    const user = await client
+      .get<UserResponse>('/auth/me', {
+        headers: {
+          cookie: `connect.sid=${sessionId}`,
+        },
+      })
+      .then((res) => res.data.data);
+    return user;
+  } catch (error) {
+    return null;
+  }
+};
