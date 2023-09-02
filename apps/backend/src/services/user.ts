50e4418140e2a6065807cb0120dbe424ef188338
Karim <devkarim@hotmail.com>
2023-09-02 03:58:51 +0300

    feat(backend): add isUsernameTaken function

--- apps/backend/src/services/user.ts
+++ apps/backend/src/services/user.ts
@@ -1,11 +1,11 @@
 import prisma from '@/lib/prisma';
 import { hash } from '@/lib/hash';
 import { exclude } from '@/lib/exclude-prisma';
-import { CreateUser } from '@/schema/user.schema';
 
 import { User } from 'database';
+import { RegisterSchema } from 'models';
 
-export const createUser = async (data: CreateUser) => {
+export const createUser = async (data: RegisterSchema) => {
   const hashedPassword = await hash(data.password);
   return prisma.user.create({
     data: { ...data, password: hashedPassword },
@@ -16,16 +16,23 @@ export const createUser = async (data: CreateUser) => {
 export const getFullUserByEmail = async (email: string) =>
   prisma.user.findUnique({ where: { email } });
 
+export const getFullUserByUsername = async (username: string) =>
+  prisma.user.findUnique({ where: { username } });
+
 export const getFullUserById = async (id: number) =>
   prisma.user.findUnique({ where: { id } });
 
 export const isEmailTaken = async (email: string) =>
   !!(await getFullUserByEmail(email));
 
+export const isUsernameTaken = async (username: string) =>
+  !!(await getFullUserByUsername(username));
+
 export const parseSession = (user: User): Express.User => {
   return {
     id: user.id,
     email: user.email,
     name: user.name,
+    username: user.username,
   };
 };
