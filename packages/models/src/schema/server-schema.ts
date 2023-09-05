d132a14ab5ab337e7d4110c12d5d1bca4abe5566
Karim <devkarim@hotmail.com>
2023-09-06 02:47:17 +0300

    feat(models): remove inviteCode from server create

--- packages/models/src/schema/server-schema.ts
+++ packages/models/src/schema/server-schema.ts
@@ -6,14 +6,6 @@ export const createServerSchema = z.object({
     .string({ required_error: Messages.required.server.name })
     .min(Limits.server.name.min, { message: Messages.limits.server.name.min })
     .max(Limits.server.name.max, { message: Messages.limits.server.name.max }),
-  inviteCode: z
-    .string({ required_error: Messages.required.server.inviteCode })
-    .min(Limits.server.inviteCode.min, {
-      message: Messages.limits.server.inviteCode,
-    })
-    .max(Limits.server.inviteCode.max, {
-      message: Messages.limits.server.inviteCode,
-    }),
 });
 
 export type CreateServerSchema = z.infer<typeof createServerSchema>;
