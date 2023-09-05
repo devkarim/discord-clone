11bcac5124fdea6dbbc2c0d82b2af7f6989b0f3f
Karim <devkarim@hotmail.com>
2023-09-06 02:49:26 +0300

    feat(web): add query provider component

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Open_Sans } from 'next/font/google';
 
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import ToastProvider from '@/components/providers/toast-provider';
+import QueryProvider from '@/components/providers/query-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -28,8 +29,10 @@ export default function RootLayout({
           enableSystem={false}
           storageKey="discord-theme"
         >
-          <ToastProvider />
-          {children}
+          <QueryProvider>
+            <ToastProvider />
+            {children}
+          </QueryProvider>
         </ThemeProvider>
       </body>
     </html>
