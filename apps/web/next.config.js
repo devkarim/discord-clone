e3d22d068370a47ae383a2de4dbed8da97a31575
Karim <devkarim@hotmail.com>
2023-09-05 23:14:26 +0300

    fix(web): transpile utils & models shared packages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['database'],
+  transpilePackages: ['database', 'utils', 'models'],
 };
 
 module.exports = nextConfig;
