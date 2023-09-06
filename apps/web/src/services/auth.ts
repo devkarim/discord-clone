96ce54797339d6d8a958116975228301f3f37171
Karim <devkarim@hotmail.com>
2023-09-07 01:37:38 +0300

    feat(web): add getMe function
    Add getMe function that gets current user logged in.

--- apps/web/src/services/auth.ts
+++ apps/web/src/services/auth.ts
@@ -1,9 +1,10 @@
-import { User } from 'database';
+import { User, UserSession } from 'database';
 import { LoginSchema, BaseResponse, RegisterSchema } from 'models';
 
 import client from './client';
 
 type UserResponse = BaseResponse<Omit<User, 'password'>>;
+type UserSessionResponse = BaseResponse<UserSession>;
 
 export const login = async (formData: LoginSchema) =>
   client
@@ -14,3 +15,6 @@ export const register = async (formData: RegisterSchema) =>
   client
     .post<UserResponse>('/auth/register', formData)
     .then((res) => res.data.data);
+
+export const getMe = async () =>
+  client.get<UserSessionResponse>('/auth/me').then((res) => res.data.data);
