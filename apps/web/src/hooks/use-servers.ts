3d57b3ad38bb1e82e59e91820579a3b7296a000d
Karim <devkarim@hotmail.com>
2023-09-07 01:37:00 +0300

    fix(web): use ".ts" extension for hooks
    Fix hooks files using ".tsx" instead of ".ts"

--- apps/web/src/hooks/use-servers.ts
+++ apps/web/src/hooks/use-servers.ts
@@ -1,5 +1,6 @@
-import { getUserServers } from '@/services/server';
 import { useQuery } from '@tanstack/react-query';
+
+import { getUserServers } from '@/services/server';
 
 const useClientServers = () => {
   const query = useQuery({ queryKey: ['servers'], queryFn: getUserServers });
