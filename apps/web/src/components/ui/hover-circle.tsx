7300ab6a9c60237b60d8d131dde2f4bdfed1399c
Karim <devkarim@hotmail.com>
2023-09-06 22:40:15 +0300

    feat(web): add active prop to hover-circle
    Add active prop to hover-circle component that does exact same thing
    when hovered.

--- apps/web/src/components/ui/hover-circle.tsx
+++ apps/web/src/components/ui/hover-circle.tsx
@@ -10,6 +10,7 @@ interface HoverCircleProps extends React.HTMLAttributes<HTMLDivElement> {
   parentClassName?: string;
   showIndiaction?: boolean;
   tooltip?: React.ReactNode;
+  active?: boolean;
 }
 
 const HoverCircle: React.FC<HoverCircleProps> = ({
@@ -17,6 +18,7 @@ const HoverCircle: React.FC<HoverCircleProps> = ({
   className,
   tooltip,
   showIndiaction = true,
+  active,
   ...props
 }) => {
   return (
@@ -32,12 +34,18 @@ const HoverCircle: React.FC<HoverCircleProps> = ({
             <div
               className={cn(
                 'flex flex-col justify-center items-center relative overflow-hidden peer h-16 w-16 bg-background hover:bg-primary hover:text-foreground rounded-[32px] hover:rounded-2xl transition-all cursor-pointer duration-300',
+                { 'rounded-2xl bg-primary text-foreground': active },
                 className
               )}
               {...props}
             />
             {showIndiaction && (
-              <span className="absolute top-1/3 -left-[1rem] rounded-xl h-8 scale-y-0 bg-foreground peer-hover:scale-y-110 -translate-x-2 peer-hover:translate-x-0 w-4 transition-[transform] duration-300"></span>
+              <span
+                className={cn(
+                  'absolute top-1/3 -left-[1rem] rounded-xl h-8 scale-y-0 bg-foreground peer-hover:scale-y-110 -translate-x-2 peer-hover:translate-x-0 w-4 transition-[transform] duration-300',
+                  { 'scale-y-110 translate-x-0': active }
+                )}
+              ></span>
             )}
           </TooltipTrigger>
           {tooltip && (
