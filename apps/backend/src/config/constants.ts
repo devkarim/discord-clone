b5f19231fbca58d667c9cf883986ee74acce28cc
Karim <devkarim@hotmail.com>
2023-09-07 02:10:56 +0300

    fix(backend): resolve domains constants

--- apps/backend/src/config/constants.ts
+++ apps/backend/src/config/constants.ts
@@ -5,11 +5,13 @@ export const isProduction = env.NODE_ENV === 'production';
 
 export const API_DOMAIN = isDevelopment
   ? 'localhost'
-  : 'api.discord-clone.karimwael.com';
-export const APP_DOMAIN = isDevelopment ? 'localhost' : 'discord.karimwael.com';
+  : 'api.discord.karimwael.com';
+export const APP_DOMAIN = isDevelopment
+  ? 'localhost'
+  : 'discord-clone.karimwael.com';
 export const API_URL = isDevelopment
   ? `http://${API_DOMAIN}:8000`
   : `https://${API_DOMAIN}`;
 export const APP_URL = isDevelopment
   ? `http://${APP_DOMAIN}:3000`
-  : `https://${APP_DOMAIN}.com`;
+  : `https://${APP_DOMAIN}`;
