3318b2586ae010d8315df6bbe95b37c156889570
Karim <devkarim@hotmail.com>
2023-09-08 00:57:48 +0300

    feat(web): add icon-button component
    Add icon-button component for showing icon as a button with tooltip
    and hover effects.

---
+++ apps/web/src/components/ui/icon-button.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useState } from 'react';
+
+import { cn } from '@/lib/utils';
+
+import ActionTooltip from './action-tooltip';
+
+interface IconButtonProps {
+  className?: string;
+  onClick?: () => void;
+  onHover?: () => void;
+  tooltip?: React.ReactNode;
+  side?: 'bottom' | 'top' | 'right' | 'left' | undefined;
+  bg?: boolean;
+  children: React.ReactNode;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  className,
+  onClick,
+  onHover,
+  tooltip,
+  side,
+  bg = false,
+  children,
+}) => {
+  const [isMouseOut, setIsMouseOut] = useState(true);
+
+  return (
+    <ActionTooltip label={tooltip} side={side} sideOffset={12}>
+      <p
+        className={cn(
+          'cursor-pointer opacity-60 hover:opacity-100 transition-opacity',
+          bg && 'p-2 hover:bg-foreground/10 rounded-lg transition-colors',
+          className
+        )}
+        onMouseEnter={() => {
+          if (!isMouseOut) return;
+          setIsMouseOut(false);
+          onHover?.();
+        }}
+        onMouseLeave={() => setIsMouseOut(true)}
+        onClick={onClick}
+      >
+        {children}
+      </p>
+    </ActionTooltip>
+  );
+};
+
+export default IconButton;
