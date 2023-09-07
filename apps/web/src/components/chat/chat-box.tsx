01455dadd3ef83834b5466535b915e90f48120e7
Karim <devkarim@hotmail.com>
2023-09-08 01:06:53 +0300

    feat(web): add chat-box component
    Add chat-box component that displays an input for the user to type and
    emoji picker to choose emojis from that then get inserted to the input.

---
+++ apps/web/src/components/chat/chat-box.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useRef } from 'react';
+import { BiSolidPlusCircle } from '@react-icons/all-files/bi/BiSolidPlusCircle';
+
+import { Input } from '@/components/ui/input';
+import IconButton from '@/components/ui/icon-button';
+
+import EmojiPicker from './emoji-picker';
+
+interface ChatBoxProps {}
+
+const ChatBox: React.FC<ChatBoxProps> = ({}) => {
+  const chatInput = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="flex gap-3 items-center m-6 bg-foreground/5 py-2 px-4 rounded-xl">
+      <IconButton className="text-3xl">
+        <BiSolidPlusCircle />
+      </IconButton>
+      <Input
+        ref={chatInput}
+        className="border-0 text-lg focus-visible:ring-0 focus-visible:ring-offset-0 bg-transparent placeholder:font-light placeholder:text-base placeholder:tracking-wider"
+        placeholder="Message with @Karim"
+      />
+      <EmojiPicker
+        onEmojiSelect={(emoji) => {
+          if (!chatInput.current) return;
+          chatInput.current.value += emoji.native;
+        }}
+      />
+    </div>
+  );
+};
+
+export default ChatBox;
