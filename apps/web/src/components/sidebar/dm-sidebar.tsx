f14fb434b8bfe4ed3bf00d4797fb0723455ae739
Karim <devkarim@hotmail.com>
2023-09-08 01:03:40 +0300

    refactor(web): add icon-button to dm-sidebar

--- apps/web/src/components/sidebar/dm-sidebar.tsx
+++ apps/web/src/components/sidebar/dm-sidebar.tsx
@@ -1,21 +1,22 @@
 import { FaPlus } from '@react-icons/all-files/fa6/FaPlus';
 
 import DMList from '@/components/dm/dm-list';
+import IconButton from '@/components/ui/icon-button';
 import ClientControl from '@/components/ui/client-control';
 
 interface DMSidebarProps {}
 
 const DMSidebar: React.FC<DMSidebarProps> = ({}) => {
   return (
-    <div className="bg-channels h-full min-w-[16rem] lg:w-fit flex flex-col">
+    <div className="bg-channels h-full w-[17rem] flex flex-col select-none">
       <div className="h-full flex-grow overflow-auto px-2 pt-4 space-y-3">
         <div className="flex justify-between items-center font-semibold group transition-opacity px-3">
           <span className="text-sm opacity-60 group-hover:opacity-100  uppercase">
             Direct Messages
           </span>
-          <span className="align-middle opacity-60 hover:opacity-100 cursor-pointer">
+          <IconButton tooltip="Create DM" side="bottom">
             <FaPlus />
-          </span>
+          </IconButton>
         </div>
         <DMList />
       </div>
