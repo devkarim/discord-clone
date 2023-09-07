fce1297cbc55e40b90a200319122f9f942a2608e
Karim <devkarim@hotmail.com>
2023-09-08 02:26:00 +0300

    refactor(web): clean components in dm-sidebar

--- apps/web/src/components/sidebar/dm-sidebar.tsx
+++ apps/web/src/components/sidebar/dm-sidebar.tsx
@@ -1,27 +1,25 @@
-import { FaPlus } from '@react-icons/all-files/fa6/FaPlus';
-
 import DMList from '@/components/dm/dm-list';
-import IconButton from '@/components/ui/icon-button';
 import ClientControl from '@/components/ui/client-control';
+
+import SidebarContainer from './sidebar-container';
+import Content from './content';
+import SidebarSubHeader from './sidebar-sub-header';
 
 interface DMSidebarProps {}
 
 const DMSidebar: React.FC<DMSidebarProps> = ({}) => {
   return (
-    <div className="bg-channels h-full w-[17rem] flex flex-col select-none">
-      <div className="h-full flex-grow overflow-auto px-2 pt-4 space-y-3">
-        <div className="flex justify-between items-center font-semibold group transition-opacity px-3">
-          <span className="text-sm opacity-60 group-hover:opacity-100  uppercase">
-            Direct Messages
-          </span>
-          <IconButton tooltip="Create DM" side="bottom">
-            <FaPlus />
-          </IconButton>
-        </div>
+    <SidebarContainer>
+      <Content>
+        <SidebarSubHeader
+          label="Direct Messages"
+          tooltip="Create DM"
+          showAddButton
+        />
         <DMList />
-      </div>
+      </Content>
       <ClientControl />
-    </div>
+    </SidebarContainer>
   );
 };
 
