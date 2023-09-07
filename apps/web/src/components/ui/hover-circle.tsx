a99f69eb64704df10c6956756fb9d4b180c6ce05
Karim <devkarim@hotmail.com>
2023-09-08 00:58:21 +0300

    refactor(web): use action-tooltip in hover-circle

--- apps/web/src/components/ui/hover-circle.tsx
+++ apps/web/src/components/ui/hover-circle.tsx
@@ -1,13 +1,10 @@
 'use client';
 
-import { cn } from '@/lib/utils';
-import {
-  TooltipProvider,
-  Tooltip,
-  TooltipTrigger,
-  TooltipContent,
-} from './tooltip';
 import { usePathname } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+
+import ActionTooltip from './action-tooltip';
 
 interface HoverCircleProps extends React.HTMLAttributes<HTMLDivElement> {
   parentClassName?: string;
@@ -39,40 +36,35 @@ const HoverCircle: React.FC<HoverCircleProps> = ({
         parentClassName
       )}
     >
-      <TooltipProvider>
-        <Tooltip delayDuration={0} disableHoverableContent>
-          <TooltipTrigger>
-            <div
-              className={cn(
-                'flex flex-col justify-center items-center relative overflow-hidden peer h-16 w-16 bg-background hover:bg-primary hover:text-foreground rounded-[32px] hover:rounded-2xl transition-all cursor-pointer duration-300',
-                {
-                  'rounded-2xl bg-primary text-foreground':
-                    isActive && !activeClassName,
-                },
-                activeClassName && { [activeClassName]: isActive },
-                className
-              )}
-              {...props}
-            />
-            {showIndiaction && (
-              <span
-                className={cn(
-                  'absolute top-1/3 -left-[1rem] rounded-xl h-8 scale-y-0 bg-foreground peer-hover:scale-y-110 -translate-x-2 peer-hover:translate-x-0 w-4 transition-[transform] duration-300',
-                  {
-                    'scale-y-150 translate-x-0 peer-hover:scale-y-150':
-                      isActive,
-                  }
-                )}
-              ></span>
-            )}
-          </TooltipTrigger>
-          {tooltip && (
-            <TooltipContent sideOffset={12} side="right" className="text-md">
-              {tooltip}
-            </TooltipContent>
+      <ActionTooltip
+        label={tooltip}
+        sideOffset={12}
+        side="right"
+        className="text-base"
+      >
+        <div
+          className={cn(
+            'flex flex-col justify-center items-center relative overflow-hidden peer h-16 w-16 bg-background hover:bg-primary hover:text-foreground rounded-[32px] hover:rounded-2xl transition-all cursor-pointer duration-300',
+            {
+              'rounded-2xl bg-primary text-foreground':
+                isActive && !activeClassName,
+            },
+            activeClassName && { [activeClassName]: isActive },
+            className
           )}
-        </Tooltip>
-      </TooltipProvider>
+          {...props}
+        />
+        {showIndiaction && (
+          <span
+            className={cn(
+              'absolute top-1/3 -left-[1rem] rounded-xl h-8 scale-y-0 bg-foreground peer-hover:scale-y-110 -translate-x-2 peer-hover:translate-x-0 w-4 transition-[transform] duration-300',
+              {
+                'scale-y-150 translate-x-0 peer-hover:scale-y-150': isActive,
+              }
+            )}
+          />
+        )}
+      </ActionTooltip>
     </div>
   );
 };
