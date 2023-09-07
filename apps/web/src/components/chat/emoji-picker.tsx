b6b3654f91f07cabed3eed50bc822d1299da7516
Karim <devkarim@hotmail.com>
2023-09-08 01:04:34 +0300

    feat(web): add emoji-picker component
    Add emoji-picker component that is a popover showing emoji picker with
    some animations on hover.

---
+++ apps/web/src/components/chat/emoji-picker.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import data from '@emoji-mart/data';
+import Picker from '@emoji-mart/react';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { EMOJIS } from '@/config/data';
+import IconButton from '@/components/ui/icon-button';
+
+interface Emoji {
+  id: string;
+  keywords: string[];
+  name: string;
+  native: string;
+  shortcodes: string;
+  unified: string;
+}
+
+interface EmojiPickerProps {
+  onEmojiSelect?: (emoji: Emoji) => void;
+}
+
+const EmojiPicker: React.FC<EmojiPickerProps> = ({ onEmojiSelect }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [emojiIndex, setEmojiIndex] = useState<number>(0);
+  const { resolvedTheme } = useTheme();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  const onHover = () => {
+    if (isOpen) return false;
+    const newIndex = emojiIndex + 1;
+    setEmojiIndex(EMOJIS[newIndex] ? newIndex : 0);
+  };
+
+  return (
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
+      <PopoverTrigger>
+        <IconButton
+          className={cn(
+            'text-2xl opacity-100 text-foreground/80 hover:text-foreground/100 transition-all hover:scale-125 grayscale hover:grayscale-0',
+            isOpen && 'text-foreground/100 scale-125 grayscale-0'
+          )}
+          onHover={onHover}
+          onClick={() => setIsOpen(true)}
+        >
+          {EMOJIS[emojiIndex]}
+        </IconButton>
+      </PopoverTrigger>
+      <PopoverContent
+        side="top"
+        className="bg-transparent border-none shadow-none drop-shadow-none w-fit"
+        sideOffset={12}
+      >
+        <Picker
+          theme={resolvedTheme}
+          data={data}
+          onEmojiSelect={onEmojiSelect}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default EmojiPicker;
