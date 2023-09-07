7efd69cfe8eac8ff45a54777cfb0a756d3be3cbf
Karim <devkarim@hotmail.com>
2023-09-07 22:40:21 +0300

    feat(web): setup channel sidebar component

---
+++ apps/web/src/components/sidebar/channel-sidebar.tsx
@@ -0,0 +1,9 @@
+interface ChannelSidebarProps {
+  serverId: string;
+}
+
+const ChannelSidebar: React.FC<ChannelSidebarProps> = ({ serverId }) => {
+  return <div>Channel Sidebar</div>;
+};
+
+export default ChannelSidebar;
