16babd55796b4b92bb77ab3c7bfb61a6a273c27a
Karim <devkarim@hotmail.com>
2023-09-07 22:33:27 +0300

    refactor(web): make dm-sidebar cleaner to user

--- apps/web/src/components/sidebar/dm-sidebar.tsx
+++ apps/web/src/components/sidebar/dm-sidebar.tsx
@@ -7,13 +7,13 @@ interface DMSidebarProps {}
 
 const DMSidebar: React.FC<DMSidebarProps> = ({}) => {
   return (
-    <div className="bg-channels h-full w-fit flex flex-col">
+    <div className="bg-channels h-full min-w-[16rem] lg:w-fit flex flex-col">
       <div className="h-full flex-grow overflow-auto px-2 pt-4 space-y-3">
-        <div className="space-x-24 font-semibold group transition-opacity px-3">
-          <span className="text-sm opacity-60 inline-block group-hover:opacity-100  uppercase">
+        <div className="flex justify-between items-center font-semibold group transition-opacity px-3">
+          <span className="text-sm opacity-60 group-hover:opacity-100  uppercase">
             Direct Messages
           </span>
-          <span className="inline-block align-middle opacity-60 hover:opacity-100 cursor-pointer">
+          <span className="align-middle opacity-60 hover:opacity-100 cursor-pointer">
             <FaPlus />
           </span>
         </div>
