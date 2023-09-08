2d8eb6e5a6b8976cd301319ad220204e0f249fea
Karim <devkarim@hotmail.com>
2023-09-09 01:21:47 +0300

    feat(backend): add more server services functions
    - Create a default channel on server creation + default role for owner
    - Get user servers by checking if the user is a member of the server
    - Add getServerById function
    - Add getServerByCode function that gets the server by invite code
    - Add isUserInServer function that checks if a user is in a server
    - Add getServerByCodeAndNotUser function
    - Add updateServer function
    - Add getFreeCode function that generates a unique invite code
    - Add addMemberToServer function

--- apps/backend/src/services/server.ts
+++ apps/backend/src/services/server.ts
@@ -1,18 +1,58 @@
-import { CreateServerSchema } from 'models';
+import { CreateServerSchema, Exception, UpdateServerSchema } from 'models';
 
 import prisma from '../lib/prisma.js';
+import { generateCode } from '../lib/utils.js';
 
 export const createServer = (
   userId: number,
   code: string,
   data: CreateServerSchema
 ) =>
-  prisma.server.create({
-    data: {
-      ...data,
-      inviteCode: code,
-      ownerId: userId,
-    },
+  prisma.$transaction(async (tx) => {
+    const server = await tx.server.create({
+      data: {
+        ...data,
+        inviteCode: code,
+        ownerId: userId,
+        channels: {
+          create: {
+            name: 'general',
+            type: 'TEXT',
+            ownerId: userId,
+          },
+        },
+        roles: {
+          create: {
+            name: 'Owner',
+            permissions: {
+              create: {
+                type: 'OWNER',
+              },
+            },
+          },
+        },
+      },
+      include: { roles: true },
+    });
+
+    if (!server.roles[0].id)
+      throw Exception.manual('[ROLE_NOT_CREATED] Internal server error', 500);
+
+    await tx.server.update({
+      where: {
+        id: server.id,
+      },
+      data: {
+        members: {
+          create: {
+            userId,
+            roleId: server.roles[0].id,
+          },
+        },
+      },
+    });
+
+    return server;
   });
 
 export const getServerByInviteCode = (inviteCode: string) =>
@@ -25,9 +65,98 @@ export const getServerByInviteCode = (inviteCode: string) =>
 export const getUserServers = (userId: number) =>
   prisma.server.findMany({
     where: {
-      ownerId: userId,
+      members: {
+        some: {
+          userId,
+        },
+      },
     },
   });
 
-export const getServerById = (id: number) =>
-  prisma.server.findUnique({ where: { id } });
+export const getServerById = (userId: number, id: number) =>
+  prisma.server.findUnique({
+    where: {
+      id,
+      members: {
+        some: {
+          userId,
+        },
+      },
+    },
+    include: {
+      categories: true,
+      channels: true,
+    },
+  });
+
+export const getServerByCode = (inviteCode: string) =>
+  prisma.server.findUnique({
+    where: {
+      inviteCode,
+    },
+  });
+
+export const isUserInServer = async (userId: number, id: number) =>
+  !!(await prisma.server.findUnique({
+    where: {
+      id,
+      members: {
+        some: {
+          userId,
+        },
+      },
+    },
+  }));
+
+export const getServerByCodeAndNotUser = (userId: number, inviteCode: string) =>
+  prisma.server.findUnique({
+    where: {
+      inviteCode,
+      NOT: {
+        members: {
+          some: {
+            userId,
+          },
+        },
+      },
+    },
+  });
+
+export const updateServer = async (
+  ownerId: number,
+  id: number,
+  data: Partial<UpdateServerSchema>
+) =>
+  prisma.server.update({
+    where: {
+      id,
+      ownerId,
+    },
+    data,
+  });
+
+export const getFreeCode = async () => {
+  let code = '';
+  while (!code) {
+    const generatedCode = await generateCode();
+    const isCodeTaken = await getServerByInviteCode(generatedCode);
+    if (!isCodeTaken) {
+      code = generatedCode;
+    }
+  }
+  return code;
+};
+
+export const addMemberToServer = async (userId: number, inviteCode: string) =>
+  prisma.server.update({
+    where: {
+      inviteCode,
+    },
+    data: {
+      members: {
+        create: {
+          userId,
+        },
+      },
+    },
+  });
