c2207935f2ab70cbcce3f5d0c94415b51e8ecb10
Karim <devkarim@hotmail.com>
2023-09-09 01:27:55 +0300

    feat(web): add server services functions
    Add server services functions that correspond to backend API routes.

--- apps/web/src/services/server.ts
+++ apps/web/src/services/server.ts
@@ -1,9 +1,13 @@
 import { Server } from 'database';
 import { BaseResponse, CreateServerSchema } from 'models';
 
+import { ServerWithChannels } from '@/types/db';
+
 import client from './client';
 
 type ServerResponse = BaseResponse<Server>;
+type ServerCodeResponse = BaseResponse<{ server: Server; isInServer: boolean }>;
+type ServerWithChannelsResponse = BaseResponse<ServerWithChannels>;
 type GetServersResponse = BaseResponse<Server[]>;
 
 export const createServer = (data: CreateServerSchema) =>
@@ -15,4 +19,21 @@ export const getUserServers = () =>
   client.get<GetServersResponse>('/servers').then((res) => res.data.data);
 
 export const getServer = (id: number) =>
-  client.get<ServerResponse>(`/servers/${id}`).then((res) => res.data.data);
+  client
+    .get<ServerWithChannelsResponse>(`/servers/${id}`)
+    .then((res) => res.data.data);
+
+export const getServerByCode = (code: string) =>
+  client
+    .get<ServerCodeResponse>(`/servers/invite/${code}`)
+    .then((res) => res.data.data);
+
+export const generateInviteCode = (serverId: number) =>
+  client
+    .patch<ServerResponse>(`/servers/${serverId}/invite`)
+    .then((res) => res.data.data);
+
+export const joinServerByCode = (code: string) =>
+  client
+    .post<ServerResponse>(`/servers/join/${code}`)
+    .then((res) => res.data.data);
