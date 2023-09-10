3d36cfcaddb5e8d3ec6ee3f35037fdccd181e102
Karim <devkarim@hotmail.com>
2023-09-11 02:41:53 +0300

    feat(web): improve channel-card component
    - Add active prop to increase text opacity
    - Add onClick prop that is triggered on sidebar-card click
    - Add colors transition

--- apps/web/src/components/channel/channel-card.tsx
+++ apps/web/src/components/channel/channel-card.tsx
@@ -5,17 +5,32 @@ import { FaVideo } from '@react-icons/all-files/fa6/FaVideo';
 
 import { ChannelType } from 'database';
 
+import { cn } from '@/lib/utils';
 import Hashtag from '@/components/ui/hashtag';
 import SidebarCard from '@/components/sidebar/sidebar-card';
 
 interface ChannelCardProps {
   name: string;
   type: ChannelType;
+  active?: boolean;
+  onClick?: () => void;
 }
 
-const ChannelCard: React.FC<ChannelCardProps> = ({ name, type }) => {
+const ChannelCard: React.FC<ChannelCardProps> = ({
+  name,
+  type,
+  active,
+  onClick,
+}) => {
   return (
-    <SidebarCard className="opacity-60 hover:opacity-100 gap-2">
+    <SidebarCard
+      className={cn(
+        'text-foreground/60 hover:text-foreground/90 transition-colors gap-2',
+        active && 'text-foreground/90'
+      )}
+      active={active}
+      onClick={onClick}
+    >
       {type == 'VIDEO' ? (
         <FaVideo className="text-lg" />
       ) : type == 'VOICE' ? (
