02f1fb03d5ec9673da4e1a4e32b122d9ac6a8714
Karim <devkarim@hotmail.com>
2023-09-11 02:23:16 +0300

    feat(web): add useCurrentChannel hook
    Add useCurrentChannel hook to fetch current channel info via useParams.

---
+++ apps/web/src/hooks/use-current-channel.ts
@@ -0,0 +1,24 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+
+import { getChannel } from '@/services/channel';
+
+const useCurrentChannel = () => {
+  const { channelId } = useParams();
+
+  const query = useQuery({
+    queryKey: ['channel', channelId],
+    queryFn: () => {
+      if (channelId && typeof channelId == 'string' && !isNaN(+channelId)) {
+        return getChannel(+channelId);
+      }
+      return null;
+    },
+  });
+
+  return query;
+};
+
+export default useCurrentChannel;
