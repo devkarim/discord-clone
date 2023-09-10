eb7bfe5f093e303cb6d4bad35be62d89d65e13fd
Karim <devkarim@hotmail.com>
2023-09-11 02:44:06 +0300

    feat(web): navigate to channel on click
    Navigate to channel page on channel-card click in ChannelsList component

--- apps/web/src/components/channel/channels-list.tsx
+++ apps/web/src/components/channel/channels-list.tsx
@@ -1,4 +1,7 @@
-import { Channel } from 'database';
+import { usePathname, useRouter } from 'next/navigation';
+
+import { Channel, ChannelType } from 'database';
+
 import ChannelCard from './channel-card';
 
 interface ChannelsListProps {
@@ -6,10 +9,27 @@ interface ChannelsListProps {
 }
 
 const ChannelsList: React.FC<ChannelsListProps> = ({ channels }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const onClick = (id: number, serverId: number, type: ChannelType) => {
+    if (type == ChannelType.TEXT) {
+      router.push(`/server/${serverId}/channel/${id}`);
+    } else {
+      // TODO: Add voice & video channels support
+    }
+  };
+
   return (
     <>
       {channels.map((channel) => (
-        <ChannelCard key={channel.id} name={channel.name} type={channel.type} />
+        <ChannelCard
+          key={channel.id}
+          name={channel.name}
+          type={channel.type}
+          active={pathname.includes(`/channel/${channel.id}`)}
+          onClick={() => onClick(channel.id, channel.serverId, channel.type)}
+        />
       ))}
     </>
   );
