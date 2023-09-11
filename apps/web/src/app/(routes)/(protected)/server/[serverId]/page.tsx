1a2f701b8e9b5e174b227d2e429419a37e1eecc0
Karim <devkarim@hotmail.com>
2023-09-12 00:06:48 +0300

    feat(web): redirect to 1st channel on server page

--- apps/web/src/app/(routes)/(protected)/server/[serverId]/page.tsx
+++ apps/web/src/app/(routes)/(protected)/server/[serverId]/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import useCurrentServer from '@/hooks/use-current-server';
+import { useRouter } from 'next/navigation';
+
 interface ServerPageProps {}
 
 const ServerPage: React.FC<ServerPageProps> = ({}) => {
-  return <div></div>;
+  const router = useRouter();
+  const { data: server } = useCurrentServer();
+
+  useEffect(() => {
+    if (!server) return;
+    const channel = server.channels[0];
+    if (!channel) return;
+    router.push(`/server/${server.id}/channel/${channel.id}`);
+  }, [server, router]);
+
+  return <div className="p-6">Redirecting you to first channel...</div>;
 };
 
 export default ServerPage;
