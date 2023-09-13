b2028915bec717cb1f7344aeffa8a383c366e234
Karim <devkarim@hotmail.com>
2023-09-13 23:08:34 +0300

    refactor(web): change button success color
    - Change button success color based on dark or light mode.
    
    - Change link variant text color to be foreground.

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -17,8 +17,9 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-foreground text-background hover:bg-foreground/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
-        success: 'bg-green-500 hover:bg-green-500/90 text-white',
+        link: 'text-foreground underline-offset-4 hover:underline',
+        success:
+          'bg-green-500 hover:bg-green-500/80 dark:bg-green-700 dark:hover:bg-green-700/80 text-white',
       },
       size: {
         default: 'h-10 px-4 py-2',
