d86f3f19efe8a96d2961caf28e7715a7b2430118
Karim <devkarim@hotmail.com>
2023-09-13 23:06:26 +0300

    fix(web): allow only one file when uploading image
    Allow only one file to be uploaded in imageUploader endpoint.

--- apps/web/src/app/api/uploadthing/core.ts
+++ apps/web/src/app/api/uploadthing/core.ts
@@ -8,7 +8,7 @@ const auth = (req: Request) => getUser();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: '4MB' } })
+  imageUploader: f({ image: { maxFileSize: '4MB', maxFileCount: 1 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
@@ -22,9 +22,8 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log('Upload complete for userId:', metadata.userId);
-
-      console.log('file url', file.url);
+      // console.log('Upload complete for userId:', metadata.userId);
+      // console.log('file url', file.url);
     }),
 } satisfies FileRouter;
 
