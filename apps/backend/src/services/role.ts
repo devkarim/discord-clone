2182b4830aa4cf0cf758b7824f047bf85474304b
Karim <devkarim@hotmail.com>
2023-09-14 21:54:22 +0300

    feat(backend): add more role functions
    Add more role services functions:
    
    - getRoleById: fetches role by ID and server ID
    - addRoleToServer: adds role to server by name and permission types
    - deleteRoleFromServer: removes role from server by server ID & role ID

--- apps/backend/src/services/role.ts
+++ apps/backend/src/services/role.ts
@@ -1,4 +1,17 @@
+import { PermissionType } from 'database';
+
 import prisma from '../lib/prisma.js';
+
+export const getRoleById = (serverId: number, roleId: number) =>
+  prisma.role.findUnique({
+    where: {
+      id: roleId,
+      serverId,
+    },
+    include: {
+      permissions: true,
+    },
+  });
 
 export const getServerRoles = (userId: number, serverId: number) =>
   prisma.role.findMany({
@@ -13,9 +26,38 @@ export const getServerRoles = (userId: number, serverId: number) =>
       },
     },
     include: {
+      permissions: true,
       members: {
         include: {
           user: true,
+        },
+      },
+    },
+  });
+
+export const addRoleToServer = (
+  serverId: number,
+  name: string,
+  permissionTypes: PermissionType[]
+) =>
+  prisma.role.create({
+    data: {
+      name,
+      serverId,
+      permissions: {
+        createMany: { data: permissionTypes.map((type) => ({ type })) },
+      },
+    },
+  });
+
+export const deleteRoleFromServer = (serverId: number, roleId: number) =>
+  prisma.role.delete({
+    where: {
+      id: roleId,
+      serverId,
+      permissions: {
+        none: {
+          type: 'OWNER',
         },
       },
     },
