e1cf6919a5610b192cbdceba7052a7f3c59fbe81
Karim <devkarim@hotmail.com>
2023-09-16 20:30:42 +0300

    feat(web): add generateRandomHexColor function
    Add generateRandomHexColor utility func that generates a random color.

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const generateRandomHexColor = () =>
+  Math.floor(Math.random() * 16777215).toString(16);
