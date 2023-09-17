8236eb70f89566ed5f5688687d701863f600d095
Karim <devkarim@hotmail.com>
2023-09-17 03:37:13 +0300

    feat(models): add limits to create role schema
    Add max and min length limit for role name in create role schema.

--- packages/models/src/schema/role-schema.ts
+++ packages/models/src/schema/role-schema.ts
@@ -2,13 +2,18 @@ import { z } from 'zod';
 
 import { PermissionType } from 'database';
 
+import { Limits, Messages } from '../config';
+
 export const createRoleSchema = z.object({
-  name: z.string(),
+  name: z
+    .string()
+    .min(Limits.role.name.min, Messages.limits.role.name.min)
+    .max(Limits.role.name.max, Messages.limits.role.name.max),
   color: z
     .string()
     .regex(/^#(?:[0-9a-fA-F]{3}){1,2}$/, 'Color must be a valid hex color code')
     .optional(),
-  permissions: z.nativeEnum(PermissionType).array(),
+  permissions: z.nativeEnum(PermissionType).array().optional(),
 });
 
 export type CreateRoleSchema = z.infer<typeof createRoleSchema>;
