60c32c87fa08d96d274ea47d296851f0f001d002
Karim <devkarim@hotmail.com>
2023-09-17 03:43:35 +0300

    feat(web): add "create role" functionality
    Add functionality of creating roles in the server with a name, color and
    a list of permissions.

--- apps/web/src/components/role/create-role-form.tsx
+++ apps/web/src/components/role/create-role-form.tsx
@@ -1,7 +1,121 @@
+'use client';
+
+import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Logger } from 'utils';
+import { Exception, CreateRoleSchema, createRoleSchema } from 'models';
+
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { addServerRole } from '@/services/server';
+import { generateRandomHexColor } from '@/lib/utils';
+import useCurrentServer from '@/hooks/use-current-server';
+
+import RolePermissions from './role-permissions';
+import RoleColorPicker from './role-color-picker';
+import useCurrentRoles from '@/hooks/use-current-roles';
+
 interface CreateRoleFormProps {}
 
 const CreateRoleForm: React.FC<CreateRoleFormProps> = ({}) => {
-  return <div></div>;
+  const { refetch } = useCurrentRoles();
+  const { data: server } = useCurrentServer();
+  const [loading, setLoading] = useState(false);
+  const [isMounted, setMounted] = useState(false);
+
+  const form = useForm<CreateRoleSchema>({
+    resolver: zodResolver(createRoleSchema),
+    defaultValues: {
+      name: '',
+      color: `#${generateRandomHexColor()}`,
+    },
+  });
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  if (!isMounted) return null;
+
+  const onSubmit = async (data: CreateRoleSchema) => {
+    if (!server) return;
+    setLoading(true);
+    try {
+      await addServerRole(server.id, data);
+      toast.success(`Created ${data.name} role successfully!`);
+      refetch();
+      form.reset({
+        name: '',
+        color: data.color,
+      });
+    } catch (err) {
+      Logger.exception(err, 'create-role-form');
+      toast.error(Exception.parseError(err));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <h1 className="font-medium text-2xl">Create a role</h1>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>ROLE NAME</FormLabel>
+              <FormControl>
+                <Input type="text" disabled={loading} {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="color"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>ROLE COLOR</FormLabel>
+              <RoleColorPicker value={field.value} onChange={field.onChange} />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="permissions"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>ROLE PERMISSIONS</FormLabel>
+              <RolePermissions value={field.value} onChange={field.onChange} />
+            </FormItem>
+          )}
+        />
+        <div className="flex">
+          <Button
+            type="submit"
+            className="w-52 ml-auto"
+            variant="secondary"
+            loading={loading}
+          >
+            Create
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default CreateRoleForm;
