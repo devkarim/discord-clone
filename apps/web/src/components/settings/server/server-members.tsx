2c832297ad8b0cd2521b8b7c77e34958666b1696
Karim <devkarim@hotmail.com>
2023-09-19 20:19:19 +0300

    feat(web): list server members in server-members
    List all server members in server-members component to manage.

--- apps/web/src/components/settings/server/server-members.tsx
+++ apps/web/src/components/settings/server/server-members.tsx
@@ -1,7 +1,27 @@
+import { Skeleton } from '@/components/ui/skeleton';
+import useCurrentMembers from '@/hooks/use-current-members';
+import ServerMembersList from '@/components/member/server-members-list';
+
 interface ServerMembersProps {}
 
 const ServerMembers: React.FC<ServerMembersProps> = ({}) => {
-  return <div></div>;
+  const { data: members } = useCurrentMembers();
+
+  if (!members)
+    return (
+      <div>
+        <Skeleton className="w-full h-64" />
+      </div>
+    );
+
+  return (
+    <div className="space-y-6">
+      <p className="text-foreground/60 text-sm pt-2">
+        Manage your server members here.
+      </p>
+      <ServerMembersList members={members} />
+    </div>
+  );
 };
 
 export default ServerMembers;
