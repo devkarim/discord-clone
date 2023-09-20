d8b038f594b33b9deb183350c6b71be64365628f
Karim <devkarim@hotmail.com>
2023-09-20 20:45:59 +0300

    feat(web): add kick, ban and change role features
    Add kick, ban and change role functionalities for moderators.

--- apps/web/src/components/member/server-member-row.tsx
+++ apps/web/src/components/member/server-member-row.tsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 import { FaEllipsis } from '@react-icons/all-files/fa6/FaEllipsis';
+
+import { Logger } from 'utils';
+import { Role } from 'database';
+import { Exception } from 'models';
 
 import { FullMember } from '@/types/db';
 import ActionTooltip from '@/components/ui/action-tooltip';
@@ -17,17 +23,83 @@ import {
   DropdownMenuRadioGroup,
 } from '@/components/ui/dropdown-menu';
 import useCurrentRoles from '@/hooks/use-current-roles';
+import {
+  banServerMember,
+  changeServerMemberRole,
+  kickServerMember,
+} from '@/services/member';
+import useCurrentMembers from '@/hooks/use-current-members';
 
 interface ServerMemberRowProps {
   member: FullMember;
 }
 
 const ServerMemberRow: React.FC<ServerMemberRowProps> = ({ member }) => {
-  const { data: roles } = useCurrentRoles();
+  const [loading, setLoading] = useState(false);
+  const [currentRoleId, setCurrentRoleId] = useState<string>();
+  const { data: roles, refetch } = useCurrentRoles();
+  const { refetch: refetchMembers } = useCurrentMembers();
 
-  const kickMember = async () => {};
+  const changeMemberRole = async (newRoleId: string) => {
+    const roleId = +newRoleId;
+    if (!roleId) return toast.error("You didn't select a new role");
+    const currentRole = roles?.find((r) =>
+      r.members.some((m) => m.id === member.id)
+    );
+    const role = roles?.find((r) => r.id === roleId);
+    if (!role) return;
+    try {
+      setLoading(true);
+      if (newRoleId === currentRole?.id.toString()) {
+        await changeServerMemberRole(member.serverId, member.id);
+        setCurrentRoleId(undefined);
+        toast.success(`Changed role of ${member.user.username} to Guest`);
+      } else {
+        await changeServerMemberRole(member.serverId, member.id, roleId);
+        setCurrentRoleId(newRoleId);
+        toast.success(
+          `Changed role of ${member.user.username} to ${role.name}`
+        );
+      }
+      refetch();
+      refetchMembers();
+    } catch (err) {
+      Logger.exception(err);
+      toast.error(Exception.parseError(err));
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  const banMember = async () => {};
+  const kickMember = async () => {
+    try {
+      setLoading(true);
+      await kickServerMember(member.serverId, member.id);
+      toast.success(`Kicked ${member.user.username}`);
+      refetch();
+      refetchMembers();
+    } catch (err) {
+      Logger.exception(err);
+      toast.error(Exception.parseError(err));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const banMember = async () => {
+    try {
+      setLoading(true);
+      await banServerMember(member.serverId, member.id);
+      toast.success(`Banned ${member.user.username}`);
+      refetch();
+      refetchMembers();
+    } catch (err) {
+      Logger.exception(err);
+      toast.error(Exception.parseError(err));
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <TableRow>
@@ -48,7 +120,7 @@ const ServerMemberRow: React.FC<ServerMemberRowProps> = ({ member }) => {
           </DropdownMenuTrigger>
           <DropdownMenuContent className="p-2 w-48 text-sm space-y-2">
             <DropdownMenuSub>
-              <DropdownMenuSubTrigger>
+              <DropdownMenuSubTrigger disabled={loading}>
                 <span>Roles</span>
               </DropdownMenuSubTrigger>
               <DropdownMenuPortal>
@@ -56,9 +128,15 @@ const ServerMemberRow: React.FC<ServerMemberRowProps> = ({ member }) => {
                   className="-translate-y-1"
                   sideOffset={12}
                 >
-                  <DropdownMenuRadioGroup>
+                  <DropdownMenuRadioGroup
+                    value={currentRoleId || member.role?.id.toString()}
+                    onValueChange={changeMemberRole}
+                  >
                     {(roles || []).map((role) => (
-                      <DropdownMenuRadioItem key={role.id} value={role.name}>
+                      <DropdownMenuRadioItem
+                        key={role.id}
+                        value={role.id.toString()}
+                      >
                         <span>{role.name}</span>
                       </DropdownMenuRadioItem>
                     ))}
@@ -70,12 +148,14 @@ const ServerMemberRow: React.FC<ServerMemberRowProps> = ({ member }) => {
             <DropdownMenuItem
               className="justify-between text-red-500 focus:text-foreground focus:bg-red-500"
               onClick={kickMember}
+              disabled={loading}
             >
               <p>Kick {member.user.username}</p>
             </DropdownMenuItem>
             <DropdownMenuItem
               className="justify-between text-red-500 focus:text-foreground focus:bg-red-500"
               onClick={banMember}
+              disabled={loading}
             >
               <p>Ban {member.user.username}</p>
             </DropdownMenuItem>
