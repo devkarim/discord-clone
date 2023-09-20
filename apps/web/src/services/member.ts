e6861f436b187f10fd430b5838df8ffedade0c65
Karim <devkarim@hotmail.com>
2023-09-20 20:39:26 +0300

    feat(web): add more member service functions
    Add more member service functions:
    
    - changeServerMemberRole: makes a PATCH req to change role of member
    - kickServerMember: makes a DELETE req to kick member from server
    - banServerMember: makes a DELETE req to ban member from server

--- apps/web/src/services/member.ts
+++ apps/web/src/services/member.ts
@@ -1,15 +1,38 @@
+import { Member } from 'database';
 import { BaseResponse, BaseResponseNoData } from 'models';
 
 import { MemberWithPermissions } from '@/types/db';
 
 import client from './client';
 
-type MemberResponse = BaseResponse<MemberWithPermissions>;
+type MemberWithPermissionsResponse = BaseResponse<MemberWithPermissions>;
+type MemberResponse = BaseResponse<Member>;
 
 export const getCurrentMember = (serverId: number) =>
   client
-    .get<MemberResponse>(`/servers/${serverId}/member`)
+    .get<MemberWithPermissionsResponse>(`/servers/${serverId}/member`)
     .then((res) => res.data.data);
 
 export const leaveServer = (serverId: number) =>
   client.delete<BaseResponseNoData>(`/servers/${serverId}/leave`);
+
+export const changeServerMemberRole = (
+  serverId: number,
+  memberId: number,
+  roleId?: number
+) =>
+  client
+    .patch<MemberResponse>(`/servers/${serverId}/members/${memberId}/role`, {
+      roleId,
+    })
+    .then((res) => res.data.data);
+
+export const kickServerMember = (serverId: number, memberId: number) =>
+  client
+    .delete<BaseResponseNoData>(`/servers/${serverId}/members/${memberId}`)
+    .then((res) => res.data);
+
+export const banServerMember = (serverId: number, memberId: number) =>
+  client
+    .delete<BaseResponseNoData>(`/servers/${serverId}/members/${memberId}/ban`)
+    .then((res) => res.data);
