92967a7dc13a23ed5e115913fc7015298e1350c8
Karim <devkarim@hotmail.com>
2023-09-21 22:38:34 +0300

    feat(backend): launch socket.io server

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,39 +1,33 @@
 import 'dotenv/config';
 import './services/passport.js';
 
+import http from 'http';
 import cors from 'cors';
 import express from 'express';
 import passport from 'passport';
-import session from 'express-session';
 
 import { env } from './config/env.js';
-import redisStore from './lib/redis.js';
+import session from './lib/session.js';
 import apiRouter from './routes/api.js';
+import AppSocket from './models/socket.js';
 import errorLogger from './middlewares/error/error-logger.js';
 import errorSender from './middlewares/error/error-sender.js';
 import errorHandler from './middlewares/error/error-handler.js';
 import { API_URL, APP_URL, isProduction } from './config/constants.js';
 
 const app = express();
+const server = http.createServer(app);
+
+// Create socket server
+AppSocket.create(server);
+
+/* Middlewares for REST API */
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: APP_URL }));
 if (isProduction) app.set('trust proxy', 1);
-app.use(
-  session({
-    name: 'discord_session',
-    cookie: {
-      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
-      secure: isProduction,
-      domain: isProduction ? '.karimwael.com' : 'localhost',
-    },
-    secret: env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: redisStore,
-  })
-);
+app.use(session);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -45,6 +39,6 @@ app.use(errorLogger);
 app.use(errorSender);
 
 // Listen
-app.listen(env.PORT, () => {
+server.listen(env.PORT, () => {
   console.log(`Listening on ${API_URL}...`);
 });
