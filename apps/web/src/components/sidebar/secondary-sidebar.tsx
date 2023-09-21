c6f9bab1925f20a3a415aa621c5ae756bc5a9588
Karim <devkarim@hotmail.com>
2023-09-21 04:45:51 +0300

    fix(web): hide secondary-sidebar in explore page

--- apps/web/src/components/sidebar/secondary-sidebar.tsx
+++ apps/web/src/components/sidebar/secondary-sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useParams } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 
 import DMSidebar from './dm-sidebar';
 import ServerSidebar from './server-sidebar';
@@ -9,11 +9,16 @@ interface SecondarySidebarProps {}
 
 const SecondarySidebar: React.FC<SecondarySidebarProps> = ({}) => {
   const { serverId } = useParams();
+  const pathname = usePathname();
 
   if (serverId && typeof serverId !== 'string')
     throw new Error('serverId must be a string');
 
-  return serverId ? <ServerSidebar serverId={serverId} /> : <DMSidebar />;
+  return serverId ? (
+    <ServerSidebar serverId={serverId} />
+  ) : pathname != '/explore' ? (
+    <DMSidebar />
+  ) : null;
 };
 
 export default SecondarySidebar;
