160000d7abbb48d9cd7bf36b199522e2feb77b0a
Karim <devkarim@hotmail.com>
2023-09-21 22:37:53 +0300

    feat(backend): add socket.io handler
    Add socket.io handler for managing connected sockets.

---
+++ apps/backend/src/models/socket-handler.ts
@@ -0,0 +1,33 @@
+import { Socket } from 'socket.io';
+
+class SocketHandler {
+  private static sockets: Socket[] = [];
+  private static authSockets: Socket[] = [];
+
+  static getSocketById(id: string) {
+    return this.sockets.find((s) => s.id === id);
+  }
+
+  static getSocketByUserId(userId: number) {
+    return this.sockets.find((s) => s.request.user?.id === userId);
+  }
+
+  static addSocket(socket: Socket) {
+    this.sockets.push(socket);
+    if (socket.request.user) {
+      this.authSockets.push(socket);
+      socket.join(socket.request.user.id.toString());
+    }
+  }
+
+  static removeSocket(socket: Socket) {
+    this.sockets = this.sockets.filter((s) => s.id != socket.id);
+    this.authSockets = this.authSockets.filter((s) => s.id != socket.id);
+  }
+
+  static emitAuth(e: string, data: any) {
+    return this.authSockets.forEach((s) => s.emit(e, data));
+  }
+}
+
+export default SocketHandler;
