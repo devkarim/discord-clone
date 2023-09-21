2d4d2b54c58724d59b2cf12b661f2201df8b1815
Karim <devkarim@hotmail.com>
2023-09-21 04:49:46 +0300

    feat(web): add changing server visibilty
    Allow users to change server visibility in server-overview component
    with a checkbox. If turned on, server will be visible in explore page.

--- apps/web/src/components/settings/server/server-overview.tsx
+++ apps/web/src/components/settings/server/server-overview.tsx
@@ -13,6 +13,7 @@ import useClientServers from '@/hooks/use-servers';
 import ImageUpload from '@/components/ui/image-upload';
 import useCurrentServer from '@/hooks/use-current-server';
 import SaveChangesToast from '@/components/settings/save-changes-toast';
+import CheckboxCard from '@/components/ui/checkbox-card';
 
 interface ServerOverviewProps {}
 
@@ -20,6 +21,7 @@ const ServerOverview: React.FC<ServerOverviewProps> = ({}) => {
   const { data: server, refetch } = useCurrentServer();
   const { refetch: refetchServers } = useClientServers();
   const [name, setName] = useState<string>();
+  const [isPublic, setIsPublic] = useState<boolean>();
   const [loading, setLoading] = useState(false);
   const [saveChangesOpen, setSaveChangesOpen] = useState(false);
   const [imageUrl, setImageUrl] = useState<string>();
@@ -27,14 +29,14 @@ const ServerOverview: React.FC<ServerOverviewProps> = ({}) => {
   useEffect(() => {
     updateSaveChanges();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [name]);
+  }, [name, isPublic]);
 
   if (!server) return null;
 
   const saveChanges = async () => {
     try {
       setLoading(true);
-      await updateServer(server.id, { name, imageUrl });
+      await updateServer(server.id, { name, imageUrl, isPublic });
       setSaveChangesOpen(false);
       refetch();
       refetchServers();
@@ -54,6 +56,7 @@ const ServerOverview: React.FC<ServerOverviewProps> = ({}) => {
   const onReset = () => {
     setImageUrl(undefined);
     setName(undefined);
+    setIsPublic(undefined);
     updateSaveChanges();
     setLoading(false);
   };
@@ -62,6 +65,8 @@ const ServerOverview: React.FC<ServerOverviewProps> = ({}) => {
     if (imageUrl && imageUrl != server.imageUrl)
       return setSaveChangesOpen(true);
     if (name && name != server.name) return setSaveChangesOpen(true);
+    if (isPublic != undefined && isPublic !== server.isPublic)
+      return setSaveChangesOpen(true);
     setSaveChangesOpen(false);
   };
 
@@ -73,22 +78,30 @@ const ServerOverview: React.FC<ServerOverviewProps> = ({}) => {
         onReset={onReset}
         loading={loading}
       />
-      <div className="flex gap-12">
-        <ImageUpload
-          imageUrl={imageUrl ?? server.imageUrl}
-          name={name ?? server.name}
-          onUploadComplete={(url) => {
-            setImageUrl(url);
-            setSaveChangesOpen(true);
-          }}
-        />
-        <div className="w-72 space-y-1">
-          <Label>SERVER NAME</Label>
-          <Input
-            value={name ?? server.name}
-            onChange={(e) => onNameChange(e.currentTarget.value)}
+      <div className="space-y-12">
+        <div className="flex gap-12">
+          <ImageUpload
+            imageUrl={imageUrl ?? server.imageUrl}
+            name={name ?? server.name}
+            onUploadComplete={(url) => {
+              setImageUrl(url);
+              setSaveChangesOpen(true);
+            }}
           />
+          <div className="w-72 space-y-1">
+            <Label>SERVER NAME</Label>
+            <Input
+              value={name ?? server.name}
+              onChange={(e) => onNameChange(e.currentTarget.value)}
+            />
+          </div>
         </div>
+        <CheckboxCard
+          checked={isPublic ?? server.isPublic}
+          onCheckedChange={(checked) => setIsPublic(checked)}
+          title="Public"
+          description="By enabling this option, this server will be public to everyone in 'Explore Servers' page."
+        />
       </div>
     </div>
   );
