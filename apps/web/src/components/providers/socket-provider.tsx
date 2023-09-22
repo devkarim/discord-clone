d6772ab0d0a5a59ce2c3595f07b0f1b85a5836e4
Karim <devkarim@hotmail.com>
2023-09-22 04:52:21 +0300

    feat(web): add socket-provider component
    Add socket-provider component that wraps web app with a global socket.

---
+++ apps/web/src/components/providers/socket-provider.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+
+import useSocket from '@/hooks/use-socket';
+import { API_URL } from '@/config/constants';
+
+interface SocketProviderProps {
+  children: React.ReactNode;
+}
+
+const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+  const globalSocket = useSocket();
+
+  useEffect(() => {
+    const newSocket = io(API_URL, {
+      withCredentials: true,
+      transports: ['websocket'],
+    });
+
+    globalSocket.setSocket(newSocket);
+
+    newSocket.on('connection', () => {
+      console.log('Connected to socket');
+      globalSocket.setIsConnected(true);
+    });
+
+    newSocket.on('disconnect', () => {
+      console.log('Disconnected from socket');
+      globalSocket.setIsConnected(false);
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default SocketProvider;
