81db8e244a42b46d3b5ac5595e3b40310bebac00
Karim <devkarim@hotmail.com>
2023-09-22 04:56:30 +0300

    feat(web): add ChannelChatMessages to channel page

--- apps/web/src/app/(routes)/(protected)/server/[serverId]/channel/[channelId]/page.tsx
+++ apps/web/src/app/(routes)/(protected)/server/[serverId]/channel/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChannelChatBox from '@/components/chat/channel-chat-box';
 import MembersSidebar from '@/components/member/members-sidebar';
+import ChannelChatMessages from '@/components/chat/channel-chat-messages';
 
 interface ChannelPageProps {}
 
@@ -7,7 +8,7 @@ const ChannelPage: React.FC<ChannelPageProps> = ({}) => {
   return (
     <div className="flex h-full overflow-auto scrollbar-thin scrollbar-track-channels scrollbar-thumb-black/30 scrollbar-thumb-rounded-full">
       <div className="flex flex-col w-full h-full justify-between">
-        <div>{/* Chat */}</div>
+        <ChannelChatMessages />
         <ChannelChatBox />
       </div>
       <MembersSidebar />
