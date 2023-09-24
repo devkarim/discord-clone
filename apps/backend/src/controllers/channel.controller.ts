46377fe9ca44d05592ab7b18630d898cb1344554
Karim <devkarim@hotmail.com>
2023-09-24 04:37:32 +0300

    fix(backend): use cursor from request query
    Fix not using cursor from request query when fetching channel messages.

--- apps/backend/src/controllers/channel.controller.ts
+++ apps/backend/src/controllers/channel.controller.ts
@@ -7,9 +7,10 @@ import {
   createMessage,
   getChannelMessages,
 } from '../services/message.js';
-import channelValidator from '../validators/channel.validator.js';
+import { CHAT_ADD_KEY } from '../config/constants.js';
 import SocketHandler from '../models/socket-handler.js';
 import { getMemberByChannelId } from '../services/member.js';
+import channelValidator from '../validators/channel.validator.js';
 
 const getChannel: typeof channelValidator.checkId = async (req, res) => {
   if (!req.user) throw Errors.unauthenticated;
@@ -29,17 +30,22 @@ const sendMessage: typeof channelValidator.sendMessage = async (req, res) => {
   const member = await getMemberByChannelId(req.user.id, channelId);
   if (!member) throw Errors.server.notInServer;
   const message = await createMessage(channelId, member.id, req.body);
-  SocketHandler.emitAuth(`chat:${message.channelId}:message`, message);
+  SocketHandler.emitAuth(`${CHAT_ADD_KEY}:${message.channelId}`, message);
   return ServerResponse.success(res, message);
 };
 
-const getMessages: typeof channelValidator.checkId = async (req, res) => {
+const getMessages: typeof channelValidator.getMessages = async (req, res) => {
   if (!req.user) throw Errors.unauthenticated;
   const channelId = +req.params.id;
   if (!channelId || isNaN(channelId)) throw Errors.channel.invalidId;
+  const cursor = +(req.query.cursor ?? -1);
+  if (!cursor || isNaN(cursor)) throw Errors.invalidCursor;
   const channel = await getChannelById(req.user.id, channelId);
   if (!channel) throw Errors.channel.invalidId;
-  const messages = await getChannelMessages(channelId);
+  const messages = await getChannelMessages(
+    channelId,
+    cursor == -1 ? undefined : cursor
+  );
   return ServerResponse.success(res, {
     messages,
     cursor:
