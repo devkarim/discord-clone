359c1b7368e6257859e67ba81473bef63ebaf77e
Karim <devkarim@hotmail.com>
2023-09-24 04:32:53 +0300

    feat(web): add utility function to format date

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -7,3 +8,16 @@ export function cn(...inputs: ClassValue[]) {
 
 export const generateRandomHexColor = () =>
   Math.floor(Math.random() * 16777215).toString(16);
+
+export const formatDate = (date: Date): string => {
+  const isToday = moment(date).isSame(new Date(), 'day');
+  const isYesterday = moment(date).isSame(
+    new Date().setDate(new Date().getDate() - 1),
+    'day'
+  );
+  return isToday
+    ? 'Today at ' + moment(date).format('hh:mm A')
+    : isYesterday
+    ? 'Yesterday at ' + moment(date).format('hh:mm A')
+    : moment(date).format('DD/MM/YYYY hh:mm A');
+};
