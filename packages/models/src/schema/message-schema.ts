19de1f0dfffad3c10577ddd7acbfea50356acd77
Karim <devkarim@hotmail.com>
2023-09-27 22:25:01 +0300

    feat(models): add schema for updating message

--- packages/models/src/schema/message-schema.ts
+++ packages/models/src/schema/message-schema.ts
@@ -2,10 +2,15 @@ import { z } from 'zod';
 
 import { Messages, Limits } from '../config';
 
-export const sendMessageSchema = z.object({
+export const updateMessageSchema = z.object({
   content: z
     .string({ required_error: Messages.required.message.content })
     .min(Limits.message.min, Messages.limits.message.min),
+});
+
+export type UpdateMessageSchema = z.infer<typeof updateMessageSchema>;
+
+export const sendMessageSchema = updateMessageSchema.extend({
   fileUrl: z
     .string()
     .url({ message: Messages.required.message.fileUrl })
