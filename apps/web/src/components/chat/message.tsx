9ff3b9614e57dd8568ff83f4f95740739836c1a2
Karim <devkarim@hotmail.com>
2023-09-28 01:11:33 +0300

    feat(web): add message actions functionality
    - Add message actions functionality (edit and delete).
    - Improve message component by making smaller components.

--- apps/web/src/components/chat/message.tsx
+++ apps/web/src/components/chat/message.tsx
@@ -1,47 +1,74 @@
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { FaPen } from '@react-icons/all-files/fa/FaPen';
-import { FaTrash } from '@react-icons/all-files/fa/FaTrash';
 
-import { UpdateMessageSchema, updateMessageSchema } from 'models';
+import { UpdateMessageSchema } from 'models';
 
-import { formatDate } from '@/lib/utils';
 import Avatar from '@/components/ui/avatar';
-import { Input } from '@/components/ui/input';
 import { MessageWithAuthor } from '@/types/db';
+import { canMemberDoAction } from '@/lib/permission';
+import { formatDate, handleError } from '@/lib/utils';
+import useCurrentMember from '@/hooks/use-current-member';
 import ActionTooltip from '@/components/ui/action-tooltip';
-import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { deleteMessage, updateMessage } from '@/services/message';
+import ConfirmationModal from '@/components/modals/confirmation-modal';
+
+import MessageActions from './message-actions';
+import MessageEdit from './message-edit';
 
 interface MessageProps {
   message: MessageWithAuthor;
 }
 
 const Message: React.FC<MessageProps> = ({ message }) => {
+  const [isLoading, setLoading] = useState(false);
+  const [deleting, setDeleting] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const form = useForm<UpdateMessageSchema>({
-    resolver: zodResolver(updateMessageSchema),
-    defaultValues: { content: message.content },
-  });
+  const { data: member } = useCurrentMember();
 
-  const isLoading = form.formState.isSubmitting;
+  if (!member) return null;
 
+  const canEdit =
+    canMemberDoAction(member, 'EDIT_MESSAGES') ||
+    member.id === message.author.id;
+  const canDelete =
+    canMemberDoAction(member, 'DELETE_MESSAGES') ||
+    member.id === message.author.id;
+  const hasAccessToActions = canEdit || canDelete;
   const isEdited = message.updatedAt !== message.createdAt;
 
-  const handleDisablingEditing = () => {
-    setIsEditing(false);
-    form.reset();
-  };
-
   const saveEdit = async (data: UpdateMessageSchema) => {
-    console.log(data);
-    handleDisablingEditing();
+    try {
+      setLoading(true);
+      await updateMessage(message.id, data);
+      setIsEditing(false);
+    } catch (err) {
+      handleError(err);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const onDelete = async () => {};
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await deleteMessage(message.id);
+      setIsEditing(false);
+    } catch (err) {
+      handleError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="group relative flex gap-4 items-center hover:bg-sidebar/20 py-2 px-6">
+      <ConfirmationModal
+        isOpen={deleting}
+        onOpenChange={setDeleting}
+        title="Are you sure you want to delete this message?"
+        subtitle="By deleting this message, you will not be able to recover it."
+        onConfirm={onDelete}
+        loading={isLoading}
+      />
       <Avatar
         src={message.author.user.imageUrl}
         name={message.author.user.username}
@@ -59,83 +86,39 @@ const Message: React.FC<MessageProps> = ({ message }) => {
         </div>
         <div className="w-full space-y-1">
           {isEditing ? (
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(saveEdit)}
-                className="space-y-1"
-              >
-                <FormField
-                  name="content"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          className="w-full"
-                          onKeyDown={(e) => {
-                            if (e.key === 'Escape') handleDisablingEditing();
-                            if (e.key === 'Enter')
-                              saveEdit({ content: form.getValues('content') });
-                          }}
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <div className="flex gap-1 items-center text-xs">
-                  <p>escape to</p>
-                  <button
-                    className="text-primary hover:underline disabled:hover:no-underline disabled:opacity-70"
-                    onClick={handleDisablingEditing}
-                    type="reset"
-                    disabled={isLoading}
-                  >
-                    cancel
-                  </button>
-                  <p>or</p>
-                  <p>enter to</p>
-                  <button
-                    className="text-primary hover:underline disabled:hover:no-underline disabled:opacity-70"
-                    type="submit"
-                    disabled={isLoading}
-                  >
-                    save
-                  </button>
-                </div>
-              </form>
-            </Form>
+            <MessageEdit
+              onSaveEdit={saveEdit}
+              onCancelEdit={() => setIsEditing(false)}
+              content={message.content}
+              disabled={isLoading}
+            />
           ) : (
             <p className="break-all">
               {message.content}{' '}
               {isEdited && (
-                <span className="text-foreground/60 text-[10px] select-none">
-                  (edited)
-                </span>
+                <ActionTooltip
+                  label={formatDate(message.updatedAt)}
+                  triggerClassName="cursor-default"
+                >
+                  <span className="text-foreground/60 text-[10px] select-none">
+                    (edited)
+                  </span>
+                </ActionTooltip>
               )}
             </p>
           )}
         </div>
       </div>
-      <div className="absolute -top-5 right-5 bg-background border-[0.5px] rounded-md hidden group-hover:flex shadow-md overflow-hidden">
-        <ActionTooltip label="Edit" hidden={isLoading}>
-          <button
-            className="hover:bg-active-channel p-3 transition-colors disabled:hover:bg-transparent disabled:opacity-70"
-            onClick={() => setIsEditing(true)}
-            disabled={isLoading}
-          >
-            <FaPen />
-          </button>
-        </ActionTooltip>
-        <ActionTooltip label="Delete" hidden={isLoading}>
-          <button
-            className="hover:bg-active-channel p-3 transition-colors disabled:hover:bg-transparent text-destructive disabled:opacity-70"
-            onClick={onDelete}
-            disabled={isLoading}
-          >
-            <FaTrash />
-          </button>
-        </ActionTooltip>
-      </div>
+      {hasAccessToActions && (
+        <MessageActions
+          showEdit={canEdit}
+          showDelete={canDelete}
+          isEditDisabled={isLoading || isEditing}
+          isDeleteDisabled={isLoading}
+          onEdit={() => setIsEditing(true)}
+          onDelete={() => setDeleting(true)}
+        />
+      )}
     </div>
   );
 };
