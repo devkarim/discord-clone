05d8976da3b1a631e3cd7aa4d24f3b3bc0291607
Karim <devkarim@hotmail.com>
2023-09-28 01:05:24 +0300

    feat(web): add more message service functions
    Add more message service functions:
    
    - updateMessage: makes a PATCH req to edit message content by message ID
    
    - deleteMessage: makes a DELETE req to delete message by message ID

--- apps/web/src/services/message.ts
+++ apps/web/src/services/message.ts
@@ -1,9 +1,16 @@
-import { BaseResponse, SendMessageSchema } from 'models';
+import { Message } from 'database';
+import {
+  BaseResponse,
+  BaseResponseNoData,
+  SendMessageSchema,
+  UpdateMessageSchema,
+} from 'models';
 
 import { MessageWithAuthor } from '@/types/db';
 
 import client from './client';
 
+type MessageResponse = BaseResponse<Message>;
 type MessageWithAuthorResponse = BaseResponse<MessageWithAuthor>;
 export type MessagesWithAuthorResponse = BaseResponse<{
   messages: MessageWithAuthor[];
@@ -24,3 +31,11 @@ export const getChannelMessages = (channelId: number, cursor?: number) =>
       params: { cursor },
     })
     .then((res) => res.data.data);
+
+export const updateMessage = (id: number, data: UpdateMessageSchema) =>
+  client
+    .patch<MessageResponse>(`/messages/${id}`, data)
+    .then((res) => res.data.data);
+
+export const deleteMessage = (id: number) =>
+  client.delete<BaseResponseNoData>(`/messages/${id}`).then((res) => res.data);
