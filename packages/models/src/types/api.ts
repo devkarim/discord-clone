b44c166776938de538b8b3d3ba0901d8eefde96a
Karim <devkarim@hotmail.com>
2023-09-28 21:37:26 +0300

    feat(models): add socket response type

--- packages/models/src/types/api.ts
+++ packages/models/src/types/api.ts
@@ -3,3 +3,7 @@ export type BaseResponse<T> = { success: true; data: T };
 export type BaseResponseNoData = { success: true };
 
 export type ErrorResponse = { success: false; message: string };
+
+export type SocketResponse<T> = BaseResponse<T> | ErrorResponse;
+
+export type SocketResponseNoData = BaseResponseNoData | ErrorResponse;
