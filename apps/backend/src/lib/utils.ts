cb4b0f4b6fee85b37ebd72748ce78f3f1bf40617
Karim <devkarim@hotmail.com>
2023-09-28 21:39:32 +0300

    feat(backend): add handleSocketError utililty func

--- apps/backend/src/lib/utils.ts
+++ apps/backend/src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { customAlphabet } from 'nanoid/async';
 
+import { Exception, SocketResponse } from 'models';
+
 export const generateCode = customAlphabet(
   '1234567890abcdefghijklmnopqrstuvwxyz',
   6
 );
+
+export const handleSocketError = <T>(
+  tag: string,
+  err: unknown,
+  ack?: (res: SocketResponse<T>) => void
+) => {
+  console.log(`[${tag}]`, err);
+  ack?.({ success: false, message: Exception.parseError(err) });
+};
