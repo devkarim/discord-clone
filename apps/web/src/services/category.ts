afc9acadea272c963b525e44da02aa1a950d3cc5
Karim <devkarim@hotmail.com>
2023-09-30 02:49:32 +0300

    feat(web): add more category service functions
    Add more category service functions:
    
    - updateCategory: makes a PATCH req to a category by ID with data needed
    - deleteCategory: makes a DELETE req to delete a category by ID

--- apps/web/src/services/category.ts
+++ apps/web/src/services/category.ts
@@ -1,9 +1,26 @@
 import { Category } from 'database';
-import { BaseResponse, CreateCategorySchema } from 'models';
+import {
+  BaseResponse,
+  BaseResponseNoData,
+  CreateCategorySchema,
+  UpdateCategorySchema,
+} from 'models';
 
 import client from './client';
 
 type CategoryResponse = BaseResponse<Category>;
 
 export const createCategory = (serverId: number, data: CreateCategorySchema) =>
-  client.post<CategoryResponse>(`/servers/${serverId}/category`, data);
+  client
+    .post<CategoryResponse>(`/servers/${serverId}/category`, data)
+    .then((res) => res.data.data);
+
+export const updateCategory = (id: number, data: UpdateCategorySchema) =>
+  client
+    .patch<CategoryResponse>(`/categories/${id}`, data)
+    .then((res) => res.data.data);
+
+export const deleteCategory = (id: number) =>
+  client
+    .delete<BaseResponseNoData>(`/categories/${id}`)
+    .then((res) => res.data);
