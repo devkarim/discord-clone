67104bfac6a36b8d3ddcc98afcad8cbe8c806ae4
Karim <devkarim@hotmail.com>
2023-10-06 22:21:55 +0300

    refactor(web): improve message component
    Improve message component by specifying more specific props instead of
    using detailed message object as a prop. This change is made to render
    more customized / generalized messages.

--- apps/web/src/components/chat/message.tsx
+++ apps/web/src/components/chat/message.tsx
@@ -4,8 +4,6 @@ import { toast } from 'react-toastify';
 import { UpdateMessageSchema } from 'models';
 
 import Avatar from '@/components/ui/avatar';
-import { MessageWithAuthor } from '@/types/db';
-import { canMemberDoAction } from '@/lib/permission';
 import { cn, formatDate, handleError } from '@/lib/utils';
 import useCurrentMember from '@/hooks/use-current-member';
 import ActionTooltip from '@/components/ui/action-tooltip';
@@ -17,13 +15,34 @@ import MessageActions from './message-actions';
 import MessageAttachment from './message-attachment';
 
 interface MessageProps {
-  message: Omit<MessageWithAuthor, 'id'> & {
-    id?: number;
-    pendingMessageId?: string;
-  };
+  id?: number;
+  pendingMessageId?: string;
+  content: string;
+  imageUrl?: string | null;
+  username: string;
+  usernameColor?: string;
+  deleted?: boolean;
+  fileUrl?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  canEdit?: boolean;
+  canDelete?: boolean;
 }
 
-const Message: React.FC<MessageProps> = ({ message }) => {
+const Message: React.FC<MessageProps> = ({
+  id,
+  pendingMessageId,
+  content,
+  imageUrl,
+  username,
+  usernameColor,
+  canEdit,
+  canDelete,
+  deleted,
+  fileUrl,
+  updatedAt,
+  createdAt,
+}) => {
   const [isLoading, setLoading] = useState(false);
   const [deleting, setDeleting] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
@@ -31,24 +50,16 @@ const Message: React.FC<MessageProps> = ({ message }) => {
 
   if (!member) return null;
 
-  const canEdit =
-    canMemberDoAction(member, 'EDIT_MESSAGES') ||
-    member.id === message.author.id;
-  const canDelete =
-    canMemberDoAction(member, 'DELETE_MESSAGES') ||
-    member.id === message.author.id;
-  const hasAccessToActions = !message.deleted && (canEdit || canDelete);
-  const isEdited = message.updatedAt !== message.createdAt;
-  const isAttachment = !!message.fileUrl;
-
-  const user = message.author.user;
+  const hasAccessToActions = !deleted && (canEdit || canDelete);
+  const isEdited = updatedAt !== createdAt;
+  const isAttachment = !!fileUrl;
 
   const saveEdit = async (data: UpdateMessageSchema) => {
-    if (!message.id || message.pendingMessageId)
+    if (!id || pendingMessageId)
       return toast.error('This message is pending, please wait...');
     try {
       setLoading(true);
-      await updateMessage(message.id, data);
+      await updateMessage(id, data);
       setIsEditing(false);
     } catch (err) {
       handleError(err);
@@ -58,11 +69,11 @@ const Message: React.FC<MessageProps> = ({ message }) => {
   };
 
   const onDelete = async () => {
-    if (!message.id || message.pendingMessageId)
+    if (!id || pendingMessageId)
       return toast.error('This message is pending, please wait...');
     try {
       setLoading(true);
-      await deleteMessage(message.id);
+      await deleteMessage(id);
       setDeleting(false);
     } catch (err) {
       handleError(err);
@@ -72,28 +83,28 @@ const Message: React.FC<MessageProps> = ({ message }) => {
   };
 
   const renderMessageContent = () => {
-    if (message.deleted)
+    if (deleted)
       return (
         <p className="italic text-sm text-foreground/60 select-none">
           This message was deleted.
         </p>
       );
-    if (message.fileUrl) return <MessageAttachment fileUrl={message.fileUrl} />;
+    if (fileUrl) return <MessageAttachment fileUrl={fileUrl} />;
     if (isEditing)
       return (
         <MessageEdit
           onSaveEdit={saveEdit}
           onCancelEdit={() => setIsEditing(false)}
-          content={message.content}
+          content={content}
           disabled={isLoading}
         />
       );
     return (
       <p className="break-all">
-        {message.content}{' '}
+        {content}{' '}
         {isEdited && (
           <ActionTooltip
-            label={formatDate(message.updatedAt)}
+            label={formatDate(updatedAt)}
             triggerClassName="cursor-default"
           >
             <span className="text-foreground/60 text-[10px] select-none">
@@ -118,12 +129,12 @@ const Message: React.FC<MessageProps> = ({ message }) => {
       <div
         className={cn(
           'group relative flex gap-4 hover:bg-sidebar/20 py-2 px-6',
-          message.pendingMessageId && 'opacity-60'
+          pendingMessageId && 'opacity-60'
         )}
       >
         <Avatar
-          src={user.imageUrl}
-          name={user.username}
+          src={imageUrl}
+          name={username}
           alt="avatar"
           showStatus={false}
           parentClassName="w-12 h-12 select-none"
@@ -131,22 +142,19 @@ const Message: React.FC<MessageProps> = ({ message }) => {
         />
         <div className={cn('w-full space-y-1', isAttachment && 'space-y-2')}>
           <div className="flex items-center gap-2 select-none">
-            <p
-              className="font-semibold"
-              style={{ color: message.author.role?.color }}
-            >
-              {user.name || user.username}
+            <p className="font-semibold" style={{ color: usernameColor }}>
+              {username}
             </p>
             <p className="text-foreground/60 text-xs">
-              {formatDate(message.createdAt)}
+              {formatDate(createdAt)}
             </p>
           </div>
           <div className="w-full space-y-1">{renderMessageContent()}</div>
         </div>
         {hasAccessToActions && (
           <MessageActions
-            showEdit={canEdit && !isAttachment}
-            showDelete={canDelete}
+            showEdit={!!canEdit && !isAttachment}
+            showDelete={!!canDelete}
             isEditDisabled={isLoading || isEditing}
             isDeleteDisabled={isLoading}
             onEdit={() => setIsEditing(true)}
