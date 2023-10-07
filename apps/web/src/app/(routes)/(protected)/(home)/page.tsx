0b11ee38131033f72cf300ba1ab546ee634d9557
Karim <devkarim@hotmail.com>
2023-10-07 21:05:51 +0300

    feat(web): add introductory text to home page

--- apps/web/src/app/(routes)/(protected)/(home)/page.tsx
+++ apps/web/src/app/(routes)/(protected)/(home)/page.tsx
@@ -1,7 +1,17 @@
+import Container from '@/components/ui/container';
+
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = ({}) => {
-  return <div className="h-full"></div>;
+  return (
+    <Container className="space-y-3">
+      <h1 className="text-2xl font-semibold">Home page</h1>
+      <p>
+        Messaging your friends and family can not be easier! Choose a contact to
+        start messaging.
+      </p>
+    </Container>
+  );
 };
 
 export default HomePage;
