e94500c028c2d0f90e0e6727093b216089a1e96e
Karim <devkarim@hotmail.com>
2023-10-15 01:17:55 +0300

    feat(web): display user's mutuals
    Display user's mutuals in the sidebar.

--- apps/web/src/components/dm/dm-list.tsx
+++ apps/web/src/components/dm/dm-list.tsx
@@ -1,13 +1,30 @@
 'use client';
 
+import { useParams, useRouter } from 'next/navigation';
+
+import useMutuals from '@/hooks/use-mutuals';
+
 import DMCard from './dm-card';
 
 interface DMListProps {}
 
 const DMList: React.FC<DMListProps> = ({}) => {
+  const { data: mutuals } = useMutuals();
+  const { userId } = useParams();
+  const router = useRouter();
+
   return (
-    <div>
-      <DMCard username="Karim" />
+    <div className="space-y-1">
+      {mutuals?.map((mutual) => (
+        <DMCard
+          key={mutual.id}
+          username={mutual.username}
+          imageUrl={mutual.imageUrl}
+          status={mutual.status}
+          active={mutual.id === +userId}
+          onClick={() => router.push(`/me/${mutual.id}`)}
+        />
+      ))}
     </div>
   );
 };
