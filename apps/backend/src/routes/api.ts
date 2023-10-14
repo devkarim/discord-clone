50a1f4c5ca0fc54c52a220d78ac86c010817a9a9
Karim <devkarim@hotmail.com>
2023-10-14 21:26:08 +0300

    fix(backend): add conversation routes to apiRouter

--- apps/backend/src/routes/api.ts
+++ apps/backend/src/routes/api.ts
@@ -7,6 +7,7 @@ import inviteRouter from './invite.routes.js';
 import channelRouter from './channel.routes.js';
 import categoryRouter from './category.routes.js';
 import messageRouter from './message.routes.js';
+import conversationRouter from './conversation.routes.js';
 
 const apiRouter = express.Router();
 
@@ -21,5 +22,6 @@ apiRouter.use('/invites', inviteRouter);
 apiRouter.use('/channels', channelRouter);
 apiRouter.use('/categories', categoryRouter);
 apiRouter.use('/messages', messageRouter);
+apiRouter.use('/conversations', conversationRouter);
 
 export default apiRouter;
