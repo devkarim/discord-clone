22c9b891d1e48414390398ac0979baa2d63ae186
Karim <devkarim@hotmail.com>
2023-10-14 22:36:57 +0300

    feat(backend): add createConversation function
    Add createConversation service function for creating conversations.

--- apps/backend/src/services/conversation.ts
+++ apps/backend/src/services/conversation.ts
@@ -1,6 +1,30 @@
 import prisma from '../lib/prisma.js';
 
-import { MESSAGES_BATCH } from './message.js';
+export const createConversation = (
+  userIds: number[],
+  isGroup: boolean = false
+) =>
+  prisma.conversation.create({
+    data: {
+      isGroup,
+      users: {
+        connect: userIds.map((id) => ({
+          id,
+        })),
+      },
+    },
+    include: {
+      users: {
+        select: {
+          id: true,
+          username: true,
+          imageUrl: true,
+          name: true,
+          status: true,
+        },
+      },
+    },
+  });
 
 export const getConversationById = (id: number) =>
   prisma.conversation.findUnique({
@@ -19,6 +43,18 @@ export const getConversationById = (id: number) =>
       },
     },
   });
+
+export const getOrCreatePairConversation = async (
+  firstUserId: number,
+  secondUserId: number
+) => {
+  const conversation = await getConversationByPairOfUsers(
+    firstUserId,
+    secondUserId
+  );
+  if (!conversation) return createConversation([firstUserId, secondUserId]);
+  return conversation;
+};
 
 export const getConversationByPairOfUsers = (
   firstUserId: number,
@@ -51,35 +87,6 @@ export const getConversationByPairOfUsers = (
           username: true,
           imageUrl: true,
           name: true,
-          status: true,
-        },
-      },
-    },
-  });
-
-export const getMessagesByConversationId = (id: number, cursorTo?: number) =>
-  prisma.directMessage.findMany({
-    where: {
-      conversationId: id,
-    },
-    take: MESSAGES_BATCH,
-    skip: cursorTo != undefined ? 1 : 0,
-    cursor:
-      cursorTo != undefined
-        ? {
-            id: cursorTo,
-          }
-        : undefined,
-    orderBy: {
-      createdAt: 'desc',
-    },
-    include: {
-      author: {
-        select: {
-          id: true,
-          name: true,
-          username: true,
-          imageUrl: true,
           status: true,
         },
       },
