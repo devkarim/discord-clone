564310a637e9d872eb26c44460c627fd81f23eef
Karim <devkarim@hotmail.com>
2023-10-17 00:10:51 +0300

    feat(web): add channel-chat-box component
    Add channel-chat-box component for sending messages in channels.

--- apps/web/src/components/chat/channel-chat-box.tsx
+++ apps/web/src/components/chat/channel-chat-box.tsx
@@ -1,17 +1,59 @@
 'use client';
 
+import { toast } from 'react-toastify';
+
+import { SendMessageSchema, SocketResponse } from 'models';
+
+import { handleError } from '@/lib/utils';
+import useSocket from '@/hooks/use-socket';
+import useCurrentMember from '@/hooks/use-current-member';
 import useCurrentChannel from '@/hooks/use-current-channel';
+import usePendingMessages from '@/hooks/use-pending-messages';
 
 import ChatBox from './chat-box';
 
 interface ChannelChatBoxProps {}
 
 const ChannelChatBox: React.FC<ChannelChatBoxProps> = ({}) => {
+  const socket = useSocket((state) => state.socket);
+  const { data: member } = useCurrentMember();
+  const addPendingMessage = usePendingMessages((state) => state.addMessage);
+
   const { data: channel } = useCurrentChannel();
 
   if (!channel) return null;
 
-  return <ChatBox chatId={channel.id} name={'#' + channel.name} />;
+  const sendMessage = async (values: SendMessageSchema) => {
+    if (!socket || !socket.active)
+      return toast.error('Socket is not connected');
+    if (!member) return toast.error('You are not a member of this chat');
+    try {
+      const response: SocketResponse<{ pendingMessageId: string }> =
+        await socket.timeout(3000).emitWithAck('message', channel.id, values);
+      if (!response.success) {
+        return toast.error(response.message);
+      }
+      const createdAt = new Date();
+      const { pendingMessageId } = response.data;
+      addPendingMessage({
+        pendingMessageId,
+        authorId: member.id,
+        author: {
+          ...member,
+        },
+        channelId: channel.id,
+        createdAt,
+        updatedAt: createdAt,
+        deleted: false,
+        content: values.content,
+        fileUrl: values.fileUrl ?? null,
+      });
+    } catch (err) {
+      handleError(err);
+    }
+  };
+
+  return <ChatBox name={'#' + channel.name} sendMessage={sendMessage} />;
 };
 
 export default ChannelChatBox;
