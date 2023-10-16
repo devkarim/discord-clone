c4812a891a0741a505fcaed2c698386f80f92765
Karim <devkarim@hotmail.com>
2023-10-16 22:00:01 +0300

    feat(web): add more props to chat-welcome
    Add imageUrl and username props to chat-welcome component.

--- apps/web/src/components/chat/chat-welcome.tsx
+++ apps/web/src/components/chat/chat-welcome.tsx
@@ -1,22 +1,43 @@
-import Hashtag from '../ui/hashtag';
+import Avatar from '@/components/ui/avatar';
+import Hashtag from '@/components/ui/hashtag';
 
 interface ChatWelcomeProps {
   name: string;
+  imageUrl?: string | null;
+  username?: string;
   isChannel?: boolean;
 }
 
 const ChatWelcome: React.FC<ChatWelcomeProps> = ({
   name,
+  imageUrl,
+  username,
   isChannel = false,
 }) => {
   return (
     <div className="space-y-4 select-none px-6">
-      <div className="p-4 rounded-full bg-foreground/5 w-fit h-fit">
-        <Hashtag className="text-5xl" />
-      </div>
-      <h1 className="font-bold text-3xl">Welcome to {name}!</h1>
+      {isChannel ? (
+        <div className="p-4 rounded-full bg-foreground/5 w-fit h-fit">
+          <Hashtag className="text-5xl" />
+        </div>
+      ) : (
+        <Avatar
+          name={name}
+          src={imageUrl}
+          alt="profile-pic"
+          parentClassName="w-16 h-16"
+          firstLetterClassName="text-4xl"
+          showStatus={false}
+        />
+      )}
+      <h1 className="font-bold text-3xl">
+        {isChannel ? `Welcome to ${name}!` : name}
+      </h1>
+      {username && <h2 className="font-medium text-xl">{username}</h2>}
       <p className="text-foreground/60">
-        This is the start of {name} {isChannel ? 'channel' : 'conversation'}.
+        {isChannel
+          ? `This is the start of ${name} channel.`
+          : `This is the very beginning of your direct message history with ${name}.`}
       </p>
     </div>
   );
