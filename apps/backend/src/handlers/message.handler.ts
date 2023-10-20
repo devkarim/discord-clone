52768c464459188a9e564e8e8194ec4555dbd1d8
Karim <devkarim@hotmail.com>
2023-10-20 23:42:02 +0300

    feat(backend): send message before db creation
    Send the message to client before creating a record in the database.

--- apps/backend/src/handlers/message.handler.ts
+++ apps/backend/src/handlers/message.handler.ts
@@ -1,5 +1,5 @@
-import { nanoid } from 'nanoid';
 import { Socket } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
 import {
   Errors,
@@ -8,20 +8,25 @@ import {
   SocketResponse,
   sendMessageSchema,
 } from 'models';
+import { DirectMessage, Message } from 'database';
 
 import { handleSocketError } from '../lib/utils.js';
 import { CHAT_ADD_KEY } from '../config/constants.js';
 import { getChannelById } from '../services/channel.js';
 import SocketHandler from '../models/socket-handler.js';
 import { getMemberByChannelId } from '../services/member.js';
-import { createDirectMessage, createMessage } from '../services/message.js';
+import {
+  createCustomDirectMessage,
+  createCustomMessage,
+} from '../services/message.js';
 
 const sendMessage =
   (socket: Socket) =>
   async (
     chatId: number,
     messageValues: SendMessageSchema,
-    ack?: (res: SocketResponse<{ pendingMessageId: string }>) => void
+    memberId: number,
+    ack?: (res: SocketResponse<{ message: Message }>) => void
   ) => {
     try {
       const user = socket.request.user;
@@ -35,23 +40,43 @@ const sendMessage =
           success: false,
           message: Exception.parseError(Errors.channel.invalidId),
         });
+      if (!memberId || isNaN(memberId))
+        return ack?.({
+          success: false,
+          message: Exception.parseError(Errors.member.invalidId),
+        });
       const values = sendMessageSchema.safeParse(messageValues);
       if (!values.success)
         return ack?.({
           success: false,
           message: Exception.parseError(values.error),
         });
-      const pendingMessageId = nanoid();
-      ack?.({ success: true, data: { pendingMessageId } });
+      const id = uuidv4();
+      const createdAt = new Date();
+      const message: Message = {
+        ...values.data,
+        fileUrl: values.data.fileUrl ?? null,
+        id,
+        channelId: chatId,
+        authorId: memberId,
+        status: 'PENDING',
+        createdAt: createdAt,
+        updatedAt: createdAt,
+      };
+      ack?.({ success: true, data: { message } });
+      const author = await getMemberByChannelId(user.id, chatId);
+      if (!author || author.id !== memberId) throw Errors.server.notInServer;
+      socket.emit(`${CHAT_ADD_KEY}:${message.channelId}`, {
+        ...message,
+        status: 'SENT',
+        author,
+      });
       const channel = await getChannelById(chatId, user.id);
       if (!channel) throw Errors.channel.invalidId;
-      const member = await getMemberByChannelId(user.id, chatId);
-      if (!member) throw Errors.server.notInServer;
-      const message = await createMessage(chatId, member.id, values.data);
+      const actualMessage = await createCustomMessage(message);
       SocketHandler.emitAuth(
         `${CHAT_ADD_KEY}:${message.channelId}`,
-        message,
-        pendingMessageId
+        actualMessage
       );
     } catch (err) {
       handleSocketError('sendMessage/message.handler', err);
@@ -63,7 +88,7 @@ const sendConversationMessage =
   async (
     chatId: number,
     messageValues: SendMessageSchema,
-    ack?: (res: SocketResponse<{ pendingMessageId: string }>) => void
+    ack?: (res: SocketResponse<{ message: DirectMessage }>) => void
   ) => {
     const user = socket.request.user;
     if (!user)
@@ -82,11 +107,34 @@ const sendConversationMessage =
         success: false,
         message: Exception.parseError(values.error),
       });
-    ack?.({ success: true, data: { pendingMessageId: '' } });
-    const message = await createDirectMessage(chatId, user.id, values.data);
+    const id = uuidv4();
+    const createdAt = new Date();
+    const message: DirectMessage = {
+      ...values.data,
+      fileUrl: values.data.fileUrl ?? null,
+      id,
+      conversationId: chatId,
+      authorId: user.id,
+      status: 'PENDING',
+      createdAt: createdAt,
+      updatedAt: createdAt,
+    };
+    ack?.({ success: true, data: { message } });
+    socket.emit(`${CHAT_ADD_KEY}:${message.conversationId}`, {
+      ...message,
+      status: 'SENT',
+      author: {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        imageUrl: user.imageUrl,
+        status: user.status,
+      },
+    });
+    const actualMessage = await createCustomDirectMessage(message);
     SocketHandler.emitAuth(
-      `${CHAT_ADD_KEY}:${message.conversationId}`,
-      message
+      `${CHAT_ADD_KEY}:${actualMessage.conversationId}`,
+      actualMessage
     );
   };
 
