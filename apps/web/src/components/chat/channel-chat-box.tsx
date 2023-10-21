d9257b8d833cc03e9c39fd827411e69e8d3d809a
Karim <devkarim@hotmail.com>
2023-10-21 19:49:56 +0300

    feat(web): add message client side
    - Make the message pending until the server
    acknowledgments back the message object.
    
    - Add message in client side when the server
    acknowledgments back the message object.

--- apps/web/src/components/chat/channel-chat-box.tsx
+++ apps/web/src/components/chat/channel-chat-box.tsx
@@ -2,24 +2,24 @@
 
 import { toast } from 'react-toastify';
 
+import { Message } from 'database';
 import { SendMessageSchema, SocketResponse } from 'models';
 
 import { handleError } from '@/lib/utils';
 import useSocket from '@/hooks/use-socket';
 import useCurrentMember from '@/hooks/use-current-member';
 import useCurrentChannel from '@/hooks/use-current-channel';
-import usePendingMessages from '@/hooks/use-pending-messages';
 
 import ChatBox from './chat-box';
+import useChatMethods from '@/hooks/use-chat-methods';
 
 interface ChannelChatBoxProps {}
 
 const ChannelChatBox: React.FC<ChannelChatBoxProps> = ({}) => {
   const socket = useSocket((state) => state.socket);
   const { data: member } = useCurrentMember();
-  const addPendingMessage = usePendingMessages((state) => state.addMessage);
-
   const { data: channel } = useCurrentChannel();
+  const { addMessage } = useChatMethods(channel?.id);
 
   if (!channel) return null;
 
@@ -28,26 +28,14 @@ const ChannelChatBox: React.FC<ChannelChatBoxProps> = ({}) => {
       return toast.error('Socket is not connected');
     if (!member) return toast.error('You are not a member of this chat');
     try {
-      const response: SocketResponse<{ pendingMessageId: string }> =
-        await socket.timeout(3000).emitWithAck('message', channel.id, values);
+      const response: SocketResponse<{ message: Message }> = await socket
+        .timeout(3000)
+        .emitWithAck('message', channel.id, values, member.id);
       if (!response.success) {
         return toast.error(response.message);
       }
-      const createdAt = new Date();
-      const { pendingMessageId } = response.data;
-      addPendingMessage({
-        pendingMessageId,
-        authorId: member.id,
-        author: {
-          ...member,
-        },
-        channelId: channel.id,
-        createdAt,
-        updatedAt: createdAt,
-        deleted: false,
-        content: values.content,
-        fileUrl: values.fileUrl ?? null,
-      });
+      const { message } = response.data;
+      addMessage({ ...message, author: member });
     } catch (err) {
       handleError(err);
     }
--- apps/web/src/components/chat/conversation-chat-box.tsx
+++ apps/web/src/components/chat/conversation-chat-box.tsx
@@ -2,6 +2,7 @@
 
 import { toast } from 'react-toastify';
 
+import { DirectMessage } from 'database';
 import { SendMessageSchema, SocketResponse } from 'models';
 
 import { handleError } from '@/lib/utils';
@@ -9,12 +10,14 @@ import useSocket from '@/hooks/use-socket';
 import useCurrentConversation from '@/hooks/use-current-conversation';
 
 import ChatBox from './chat-box';
+import useChatMethods from '@/hooks/use-chat-methods';
 
 interface ConversationChatBoxProps {}
 
 const ConversationChatBox: React.FC<ConversationChatBoxProps> = ({}) => {
   const { data } = useCurrentConversation();
   const socket = useSocket((state) => state.socket);
+  const { addMessage } = useChatMethods(data?.conversation.id);
 
   if (!data) return null;
 
@@ -22,28 +25,14 @@ const ConversationChatBox: React.FC<ConversationChatBoxProps> = ({}) => {
     if (!socket || !socket.active)
       return toast.error('Socket is not connected');
     try {
-      const response: SocketResponse<{ pendingMessageId: string }> =
-        await socket
-          .timeout(3000)
-          .emitWithAck('message:conversation', data.conversation.id, values);
+      const response: SocketResponse<{ message: DirectMessage }> = await socket
+        .timeout(3000)
+        .emitWithAck('message:conversation', data.conversation.id, values);
       if (!response.success) {
         return toast.error(response.message);
       }
-      const createdAt = new Date();
-      const { pendingMessageId } = response.data;
-      // addPendingMessage({
-      //   pendingMessageId,
-      //   authorId: member.id,
-      //   author: {
-      //     ...member,
-      //   },
-      //   channelId: channel.id,
-      //   createdAt,
-      //   updatedAt: createdAt,
-      //   deleted: false,
-      //   content: values.content,
-      //   fileUrl: values.fileUrl ?? null,
-      // });
+      const { message } = response.data;
+      addMessage({ ...message, author: data.user });
     } catch (err) {
       handleError(err);
     }
