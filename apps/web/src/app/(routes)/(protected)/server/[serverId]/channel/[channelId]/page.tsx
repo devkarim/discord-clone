afa4077bb5833390ceef8ba7f7ea9f3b7fba9194
Karim <devkarim@hotmail.com>
2023-10-23 01:09:53 +0300

    feat(web): display channel content in channel page

--- apps/web/src/app/(routes)/(protected)/server/[serverId]/channel/[channelId]/page.tsx
+++ apps/web/src/app/(routes)/(protected)/server/[serverId]/channel/[channelId]/page.tsx
@@ -1,7 +1,6 @@
 import ChatContainer from '@/components/chat/chat-container';
-import ChannelChatBox from '@/components/chat/channel-chat-box';
 import MembersSidebar from '@/components/member/members-sidebar';
-import ChannelChatMessages from '@/components/chat/channel-chat-messages';
+import ChannelContent from '@/components/channel/channel-content';
 
 interface ChannelPageProps {}
 
@@ -9,8 +8,7 @@ const ChannelPage: React.FC<ChannelPageProps> = ({}) => {
   return (
     <ChatContainer>
       <div className="flex flex-col w-full h-full justify-between">
-        <ChannelChatMessages />
-        <ChannelChatBox />
+        <ChannelContent />
       </div>
       <MembersSidebar />
     </ChatContainer>
