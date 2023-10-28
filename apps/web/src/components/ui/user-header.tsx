7021a8d64f0df81c3f926d9fd5670c94d8ddb6a8
Karim <devkarim@hotmail.com>
2023-10-28 21:00:51 +0200

    feat(web): toggle voice and video in chat header

--- apps/web/src/components/ui/user-header.tsx
+++ apps/web/src/components/ui/user-header.tsx
@@ -1,19 +1,45 @@
 'use client';
 
-import { FaPhoneVolume } from '@react-icons/all-files/fa6/FaPhoneVolume';
 import { FaVideo } from '@react-icons/all-files/fa6/FaVideo';
+import { FaPhoneVolume } from '@react-icons/all-files/fa6/FaPhoneVolume';
+
+import useConversationMedia from '@/hooks/use-conversation-media';
+import useCurrentConversation from '@/hooks/use-current-conversation';
 
 import Avatar from './avatar';
-import IconButton from './icon-button';
-import useCurrentConversation from '@/hooks/use-current-conversation';
 import { Skeleton } from './skeleton';
+import IconButton from './icon-button';
 
 interface UserHeaderProps {}
 
 const UserHeader: React.FC<UserHeaderProps> = ({}) => {
+  const conversationMedia = useConversationMedia();
   const { data } = useCurrentConversation();
 
   if (!data) return <Skeleton className="h-full w-full" />;
+
+  const onVoiceCall = () => {
+    const newVoice = !conversationMedia.isVoice;
+    conversationMedia.setVideo(false);
+    conversationMedia.setVoice(newVoice);
+    if (newVoice) {
+      conversationMedia.setConversationId(data.conversation.id);
+    } else {
+      conversationMedia.setConversationId(null);
+    }
+  };
+
+  const onVideoCall = () => {
+    const newVideo = !conversationMedia.isVideo;
+    conversationMedia.setVoice(false);
+    conversationMedia.setVideo(newVideo);
+    conversationMedia.setConversationId(data.conversation.id);
+    if (newVideo) {
+      conversationMedia.setConversationId(data.conversation.id);
+    } else {
+      conversationMedia.setConversationId(null);
+    }
+  };
 
   return (
     <div className="flex w-full px-2 justify-between">
@@ -29,10 +55,18 @@ const UserHeader: React.FC<UserHeaderProps> = ({}) => {
         <p className="font-semibold">{data.user.name || data.user.username}</p>
       </div>
       <div className="flex gap-6 text-xl">
-        <IconButton tooltip="Start Voice Call" side="bottom">
+        <IconButton
+          tooltip="Start Voice Call"
+          side="bottom"
+          onClick={onVoiceCall}
+        >
           <FaPhoneVolume />
         </IconButton>
-        <IconButton tooltip="Start Video Call" side="bottom">
+        <IconButton
+          tooltip="Start Video Call"
+          side="bottom"
+          onClick={onVideoCall}
+        >
           <FaVideo />
         </IconButton>
       </div>
